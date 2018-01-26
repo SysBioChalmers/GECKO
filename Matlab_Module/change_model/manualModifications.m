0bf70c4357e3787695df8d23c9e16646dd5a4c63
Ivan Domenzain <ivand@chalmers.se>
2018-01-26 21:06:27 +0100

    style: manualModifications script reformatting

--- Matlab_Module/change_model/manualModifications.m
+++ Matlab_Module/change_model/manualModifications.m
@@ -1,9 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % model = manualModifications(model)
 % 
-%
 % Benjam?n J. S?nchez. Last edited: 2017-10-29
-% Ivan Domenzain.      Last edited: 2018-01-25
+% Ivan Domenzain.      Last edited: 2018-01-24
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function model = manualModifications(model)
@@ -86,8 +85,9 @@ for i = 1:length(model.rxns)
     %Individual Changes:
     for j = 1:length(int_pos)
 %%%%%%%%%%%%%%%%%%%%%%%%% INITIAL MANUAL CURATION: %%%%%%%%%%%%%%%%%%%%%%%%
-         % Aconitase (P19414/EC 4.2.1.3): The rxn is represented as a two step rxn in Yeast
-         % 7.5, so the kcat must be multiplied by 2 (2015-11-05)
+         % Aconitase (P19414/EC 4.2.1.3): The rxn is represented as a two 
+         % step rxn in Yeast7.5, so the kcat must be multiplied by 2 
+         %(2015-11-05)
          if strcmpi('prot_P19414',model.mets(int_pos(j))) && ...
             (~isempty(strfind(model.rxnNames{i},'cis-aconitate(3-) to isocitrate')) || ...
              ~isempty(strfind(model.rxnNames{i},'citrate to cis-aconitate(3-) (')) || ...
@@ -96,20 +96,17 @@ for i = 1:length(model.rxns)
              model.S(int_pos(j),i) = model.S(int_pos(j),i)/2;
          end
 %%%%%%%%%%%%%%%%%%%%%%%%%% MANUAL CURATION FOR TOP GROWTH LIMITING ENZYMES:       
-           % [1.2.1.12] Kcat (Sce & natural substrate) 29 [1/s], S.A. 100 (Sce)
-%          if (strcmpi('prot_P00358',model.mets(int_pos(j))) || ...
-%              strcmpi('prot_P00359',model.mets(int_pos(j))) || ...
-%              strcmpi('prot_P00360',model.mets(int_pos(j)))) && ...
+        % [1.2.1.12] Kcat (Sce & natural substrate) 29 [1/s] is a highly 
+        % growthRate limiting value, S.A. 100 (Sce)
           if strcmpi('prot_P00359',model.mets(int_pos(j))) &&...
              ~isempty(strfind(model.rxnNames{i},...
                                'glyceraldehyde-3-phosphate dehydrogenase'))
-              %model.S(int_pos(j),i) = -(29*3600)^-1;
-              %model.S(int_pos(j),i) = -(9.1*3600)^-1;
               model.S(int_pos(j),i) = -(100*1e3/1e3*MW_set)^-1;
           end
-        % FAS (P07149+P19097/EC2.3.1.86): No kcat available in BRENDA (it was using ECs of
-        % subunits: EC3.1.2.14 and EC2.3.1.41).Value corrected with max. s.a. in S.cerevisiae
-        % for NADPH in BRENDA (2015-08-25)
+        % FAS (P07149+P19097/EC2.3.1.86): No kcat available in BRENDA 
+        % (it was using ECs of subunits: EC3.1.2.14 and EC2.3.1.41).Value 
+        % corrected with max. s.a. in S.cerevisiae for NADPH in BRENDA 
+        % (2015-08-25)
           if (strcmpi('prot_P07149',model.mets(int_pos(j)))  || ...
               strcmpi('prot_P19097',model.mets(int_pos(j))))
             if ~isempty(strfind(model.rxnNames{i},...
@@ -120,8 +117,9 @@ for i = 1:length(model.rxns)
                 model.S(int_pos(j),i) = -(3/16*60*1e3/1e3*MW_set)^-1;    %3/16 [umol/min/mg]
             end
           end
-        % Ketol-acid Reductoisomerase (P06168/EC1.1.1.86) - 2-acetyllactic acid: Substrate
-        % name in BRENDA was a synonim as name in model. Changed manually (2015-08-28).
+        % Ketol-acid Reductoisomerase (P06168/EC1.1.1.86) - 2-acetyllactic 
+        % acid: Substrate name in BRENDA was a synonim as name in model. 
+        % Changed manually (2015-08-28).
           if strcmpi('prot_P06168',model.mets(int_pos(j))) 
               if (~isempty(strfind(model.rxnNames{i},...
                                   'acetohydroxy acid isomeroreductase (')))
@@ -131,86 +129,77 @@ for i = 1:length(model.rxns)
                  model.S(int_pos(j),i) = -(78.3*3600)^-1;    %BRENDA: 2-aceto-2-hydroxybutyrate
               end     
           end
-        % Phosphoribosylformylglycinamidine synthase (P38972/EC6.3.5.3): Only kcat available
-        % in BRENDA was for NH4 in E.coli. Value corrected with max. s.a. in E.coli
+        % Phosphoribosylformylglycinamidine synthase (P38972/EC6.3.5.3): 
+        % Only kcat available in BRENDA was for NH4 in E.coli. Value 
+        % corrected with max. s.a. in E.coli
         % (2015-08-28).
           if strcmpi('prot_P38972',model.mets(int_pos(j))) && ...
              (~isempty(strfind(model.rxnNames{i},'phosphoribosylformyl glycinamidine synthetase')))
              model.S(int_pos(j),i) = -(2.15*141418*60/1000)^-1;    
           end
-        % HMG-CoA reductase (P12683-P12684/EC1.1.1.34): Only kcat available in BRENDA was
-        % for Rattus Norvegicus. Value corrected with max. s.a. in S.cerevisiae, checked in
-        % original source (I.F.Durr & H.Rudney, J. Biol. Chem. 1960 235:2572-2578
+        % HMG-CoA reductase (P12683-P12684/EC1.1.1.34): Only kcat available 
+        % in BRENDA was for Rattus Norvegicus. Value corrected with max. 
+        % s.a. in S.cerevisiae, checked in original source (I.F.Durr & 
+        % H.Rudney, J. Biol. Chem. 1960 235:2572-2578
         % http://www.jbc.org/content/235/9/2572) (2015-08-31).
           if (strcmpi('prot_P12683',model.mets(int_pos(j)))  || ...
               strcmpi('prot_P12684',model.mets(int_pos(j)))) && ...
               (~isempty(strfind(model.rxnNames{i},'hydroxymethylglutaryl')))
              model.S(int_pos(j),i) = -(13.5*0.001*60*1e3/1e3*MW_set)^-1;
-             %model.S(int_pos(j),i) = -(3.0851*3600)^-1;
           end
-          % amidophosphoribosyltransferase [P04046/EC2.4.2.14]
-          % No Kcat reported on BRENDA, the maximum S.A. (E. coli is taken
-          % instead)
+        % amidophosphoribosyltransferase [P04046/EC2.4.2.14]
+        % No Kcat reported on BRENDA, the maximum S.A. (E. coli is taken
+        % instead)
           if strcmpi('prot_P04046',model.mets(int_pos(j))) && ...
              (~isempty(strfind(model.rxnNames{i},'phosphoribosylpyrophosphate amidotransferase')))
              model.S(int_pos(j),i) = -(17.2*194000*60/1000)^-1;
           end
-         % Enolase (1&2) [4.2.1.11]
-         % 71.4 (1/s) is the Kcat reported for 2-phospho-D-glycerate
-         % 230 (1/s) is the Kcat reported for 2-phosphoglycerate
-         % both measurements are for native enzymes
-        %if (strcmpi('prot_P00924',model.mets(int_pos(j))) || ...
-        %    strcmpi('prot_P00925',model.mets(int_pos(j)))) && ...
-           if strcmpi('prot_P00924',model.mets(int_pos(j)))  && ...
-              (~isempty(strfind(model.rxnNames{i},'enolase'))) 
-                model.S(int_pos(j),i) = -1/(230*3600);
-           end
-           
-        % [4.1.1.-, 4.1.1.43, 4.1.1.72, 4.1.1.74] Pyruvate decarboxylase 
-        %  Resulted to be a growth limiting enzyme but the Kcat
-        % value seems to be the best candidate for this reaction
-        %if (strcmpi('prot_P06169',model.mets(int_pos(j))) || ...
-        %     strcmpi('prot_P26263',model.mets(int_pos(j)))) && ...
-         if strcmpi('prot_P06169',model.mets(int_pos(j))) && ...
-            (~isempty(strfind(model.rxnNames{i},'pyruvate decarboxylase')))
-            model.S(int_pos(j),i) = -1/(145*3600); 
+        % Enolase (1&2) [4.2.1.11] 71.4 (1/s) is the Kcat reported for 
+        % 2-phospho-D-glycerate
+        % 230 (1/s) is the Kcat reported for 2-phosphoglycerate
+        % both measurements are for native enzymes
+          if strcmpi('prot_P00924',model.mets(int_pos(j)))  && ...
+             (~isempty(strfind(model.rxnNames{i},'enolase'))) 
+               model.S(int_pos(j),i) = -1/(230*3600);
+          end           
+       % [4.1.1.-, 4.1.1.43, 4.1.1.72, 4.1.1.74] Pyruvate decarboxylase 
+       %  Resulted to be a growth limiting enzyme but the Kcat
+       % value seems to be the best candidate for this reaction
+        if strcmpi('prot_P06169',model.mets(int_pos(j))) && ...
+           (~isempty(strfind(model.rxnNames{i},'pyruvate decarboxylase')))
+           model.S(int_pos(j),i) = -1/(145*3600); 
+        end         
+       % 1,3-beta-glucan synthase component FKS1 (P38631/EC2.4.1.34): Retrieved value
+       % was from Staphylococcus aureus. Value changed with s.a. in S.cerevisiae
+       % from BRENDA (2017-03-05).
+         if strcmpi('prot_P38631',model.mets(int_pos(j))) && ...
+           (~isempty(strfind(model.rxnNames{i},'1,3-beta-glucan synthase (')))
+            model.S(int_pos(j),i) = -(4*60*1e3/1e3*MW_set)^-1;    %4 [umol/min/mg]
+            disp(['P38631' ' modified'])         
          end
-         
-         % 1,3-beta-glucan synthase component FKS1 (P38631/EC2.4.1.34): Retrieved value
-         % was from Staphylococcus aureus. Value changed with s.a. in S.cerevisiae
-         % from BRENDA (2017-03-05).
-          if strcmpi('prot_P38631',model.mets(int_pos(j))) && ...
-             (~isempty(strfind(model.rxnNames{i},'1,3-beta-glucan synthase (')))
-             model.S(int_pos(j),i) = -(4*60*1e3/1e3*MW_set)^-1;    %4 [umol/min/mg]
-             disp(['P38631' ' modified'])
-             
-          end
-        % [Q06817//EC6.1.1.14] glycyl-tRNA synthetase
-        %  Resulted to be a growth limiting enzyme, maximum catalytic value
-        % found by the automatic algorithm was 15.9 1/s
-        % from BRENDA (2018-01-25).
+       % [Q06817//EC6.1.1.14] glycyl-tRNA synthetase
+       %  Resulted to be a growth limiting enzyme, maximum catalytic value
+       % found by the automatic algorithm was 15.9 1/s
+       % from BRENDA (2018-01-22).
          if strcmpi('prot_Q06817',model.mets(int_pos(j))) && ...
             (~isempty(strfind(model.rxnNames{i},'glycyl-tRNA synthetase')))
             model.S(int_pos(j),i) = -1/(15.9*3600); 
-         end
-             
+         end       
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% MANUAL CURATION FOR CARBON SOURCES
-        % alpha,alpha-trehalase (P32356/EC3.2.1.28): The available kcat values were not for
-        % S.cerevisiae. Value corrected with s.a. in S.cerevisiae, checked in original source
-        % (N.Biswas &  A.K.Ghosh, BBA. 1996 1290(1):95-100) (2015-09-02)
-        if ~isempty(strfind(model.rxns{i},'r_0194'))
-            model.S(int_pos(j),i) = -(44*60*1e3/1e3*MW_set)^-1;    %44 [umol/min/mg]
-        end
-          if (strcmpi('prot_P32356',model.mets(int_pos(j)))  || ...
+       % alpha,alpha-trehalase (P32356/EC3.2.1.28): The growth on
+       % alpha,alpha-trehalaso was the extremely highly overpredicted, the
+       % minimum substrate specific kcat value (0.67 1/s) is chosen instead
+       % (Metarhizium flavoviride) 
+       % from BRENDA (2018-01-22).
+         if (strcmpi('prot_P32356',model.mets(int_pos(j)))  || ...
               strcmpi('prot_P48016',model.mets(int_pos(j)))) && ...
              (~isempty(strfind(model.rxnNames{i},'alpha,alpha-trehalase')))
              model.S(int_pos(j),i) = -(0.67*60*1e3/1e3*MW_set)^-1;    
-          end        
-       
+          end               
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% MANUAL CURATION FOR TOP USED ENZYMES:
           % atp synthase mitochondrial (P07251-P00830/EC3.6.3.14): No Kcat 
           % reported for S. cerevisiae, value for Geobacillus stearothermophilus
-          % for ATP is used instead 217 [1/s]-
+          % for ATP is used instead 217 [1/s]
           % from BRENDA (2017-01-18).
           if (strcmpi('prot_P07251',model.mets(int_pos(j))) || ...
               strcmpi('prot_P00830',model.mets(int_pos(j))))&& ...
@@ -239,8 +228,9 @@ for i = 1:length(model.rxns)
               model.S(int_pos(j),i) = -(60*75000*60/1000)^-1;
           end
            % Glutamate N-acetyltransferase (Q04728/EC2.3.1.1): Missanotated 
-           % EC (should be 2.3.1.35), and no kcats for the correct EC. 
-           % Value corrected with s.a. in S.cerevisiae from BRENDA (2015-08-31).
+           % EC (should be 2.3.1.35), and no kcats for the correct EC. were
+           % found. Value corrected with s.a. in S.cerevisiae 
+           % from BRENDA (2015-08-31).
           if strcmpi('prot_Q04728',model.mets(int_pos(j))) && ...
              (~isempty(strfind(model.rxnNames{i},'ornithine transacetylase (No1)')))
              model.S(int_pos(j),i) = -(22*60*1e3/1e3*MW_set)^-1;    %22 [umol/min/mg]
@@ -274,9 +264,8 @@ end
      model.S(strcmpi(model.mets,'prot_P14540'),pos_rxn) = 0;
      model.rxns{pos_rxn} = rxn_id(1:end-3);
  end
-% 
-% % Remove protein P40009 (Golgi apyrase) from missanotated rxns (2016-12-14):
- %disp(class('r_0227No3'))
+ 
+% Remove protein P40009 (Golgi apyrase) from missanotated rxns (2016-12-14):
 % model = removeRxns(model,{'r_0227No3'});
   pos_rxn = find(~cellfun(@isempty,strfind(model.rxnNames,'ATPase, cytosolic (No3)')));
   if ~isempty(pos_rxn) && pos_rxn~=0
@@ -333,18 +322,15 @@ end
 % Remove saved arm reactions:
 model = removeRxns(model,model.rxns(arm_pos(1:p)));
 
-
-% Possible incompatibillity with other MATLAB versions (needs to be
-% corrected).
-% %Change gene rules:
-% for i = 1:length(model.rules)
-%     if ~isempty(model.rules{i})
-%         %Change gene ids:
-%         model.rules{i} = strrep(model.rules{i},'x(','');
-%         model.rules{i} = strrep(model.rules{i},')','');
-%         model.rules{i} = model.genes{str2double(model.rules{i})};
-%     end
-% end
+%Change gene rules:
+for i = 1:length(model.rules)
+    if ~isempty(model.rules{i})
+        %Change gene ids:
+        model.rules{i} = strrep(model.rules{i},'x(','');
+        model.rules{i} = strrep(model.rules{i},')','');
+        model.rules{i} = model.genes{str2double(model.rules{i})};
+    end
+end
 
 % Remove unused enzymes after manual curation (2017-01-16):
 rem_enz = false(size(model.enzymes));
