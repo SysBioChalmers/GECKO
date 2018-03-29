1f133d13734e71459c40baf330697b2e8ef28e74
Ivan Domenzain <ivand@chalmers.se>
2018-03-29 16:01:13 +0200

    Revert "fix: Fixed bug for empty limiting Kcats cell"
    This reverts commit f97bb5ad4e31020fd564b9c0b37969a9291148b9.

--- Matlab_Module/limit_proteins/getConstrainedModel.m
+++ Matlab_Module/limit_proteins/getConstrainedModel.m
@@ -21,19 +21,16 @@ function [ecModel_batch,OptSigma] = getConstrainedModel(ecModel,sigma,Ptot,gR_ex
 	%Get a preliminary enzyme constrained model for performing the Kcats 
 	%sensitivity analysis
 	ecModel_batch = constrainEnzymes(ecModel,Ptot,sigma);
-    ObjIndex      = find(ecModel_batch.c);
 	solution      = solveLP(ecModel_batch,1);
     if ~isempty(solution.f)
         %Set the media according to the media of the experimental measurement
         cd ../Kcat_sensitivity_analysis
         c_source          = 'D-glucose exchange (reversible)';
-        %[ecModel_batch,~] = changeMedia_batch(ecModel_batch,c_source,'Min');
-        carbonUptake = 1000;%5.8;%[mmol/gDwh]
-        OxyUptake    = 1000;%13; %[mmol/gDwh]
-        [ecModel_batch,~] = fixUptakeRates(ecModel_batch,c_source,'Min',[],carbonUptake,OxyUptake);
+        [ecModel_batch,~] = changeMedia_batch(ecModel_batch,c_source,'Min');
         solution          = solveLP(ecModel_batch,1);
+        ObjIndex          = find(ecModel_batch.c);
         % If the model is overconstrained
-        if ((gR_exp-solution.x(ObjIndex))/gR_exp)>0.05 
+        if (gR_exp-solution.x(ObjIndex))>0 
             disp('***************************************************************')
             disp('                The ECmodel is overconstrained                 ')
             %Perform a sensitivity analysis on the individual Kcat coefficients,
@@ -49,23 +46,9 @@ function [ecModel_batch,OptSigma] = getConstrainedModel(ecModel,sigma,Ptot,gR_ex
         disp('***************************************************************')
         disp('        Fitting the average enzymes saturation factor          ')
         OptSigma = sigmaFitter(ecModel,Ptot,gR_exp);
-        %Avoid extreme values
-        if OptSigma == 0.4 || OptSigma == 0.6
-            OptSigma = 0.5;
-        end
         %The ecModel with new modified Kcat values is constrained with the 
         %optimal sigma value found
-        cd ../limit_proteins
         ecModel_batch = constrainEnzymes(ecModel,Ptot,OptSigma);
-        solution      = solveLP(ecModel_batch,1);
-        % If the model is overconstrained
-        if ((gR_exp-solution.x(ObjIndex))/gR_exp)>0.05
-            disp('***************************************************************')
-            disp('             Fitting the ATP growth requeriments               ')
-            biomassRxn    = ecModel_batch.rxnNames(ObjIndex-1);
-            cd ../Kcat_sensitivity_analysis
-            ecModel_batch = fitGAM(ecModel_batch,biomassRxn,gR_exp,Ptot);
-        end            
         %Simulate growth on minimal glucose media and export the top ten used 
         %enzymes to the file "topUsedEnzymes.txt" in the containing folder
         cd (current)
