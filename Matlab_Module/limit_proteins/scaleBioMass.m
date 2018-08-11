f11602465a871cae628d5c2eaa21819838b00fd2
benjasanchez <bensan@chalmers.se>
2018-08-11 14:44:04 +0200

    feat: add lipids to scaleBioMass
    difference in added protein is now split between carbs and lipids, for consistency

--- Matlab_Module/limit_proteins/scaleBioMass.m
+++ Matlab_Module/limit_proteins/scaleBioMass.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % model = scaleBioMass(model,Ptot,GAM,scale_comp)
 % 
-% Benjamín Sánchez. Last update: 2018-08-10
+% Benjamín Sánchez. Last update: 2018-08-11
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function model = scaleBioMass(model,Ptot,GAM,scale_comp)
@@ -16,32 +16,24 @@ if nargin < 4
     scale_comp = true;
 end
 
-% Compute protein and carbohydrate fractions:
-[~,Pbase,Cbase,~,~,~] = sumBioMass(model);
-fP   = Ptot/Pbase;
-Ctot = Cbase + Pbase - Ptot;
-fC   = Ctot/Cbase;
+%Compute carbohydrate and lipid new amounts, based on:
+%1. Total mass remains constant, i.e. Pbase+Cbase+Lbase = Ptot+Ctot+Ltot
+%2. Difference in mass is distributed proportionally, i.e. Ctot/Ltot = Cbase/Lbase
+[~,Pbase,Cbase,~,~,Lbase] = sumBioMass(model);
+Ctot = Cbase + (Pbase - Ptot)*Cbase/(Lbase+Cbase);
+Ltot = Lbase + (Pbase - Ptot)*Lbase/(Lbase+Cbase);
 
+%Compute rescaling fractions:
+fP = Ptot/Pbase;
+fC = Ctot/Cbase;
+fL = Ltot/Lbase;
+
+%Change compositions:
 if scale_comp
-    %Change protein composition:
-    pr_pos = strcmp(model.rxnNames,'protein pseudoreaction');
-    for i = 1:length(model.mets)
-        S_ip   = model.S(i,pr_pos);
-        isProd = strcmp(model.metNames{i},'protein');
-        if S_ip ~= 0 && ~isProd
-            model.S(i,pr_pos) = fP*S_ip;
-        end
-    end
-    
-    %Change carbohydrate composition:
-    cr_pos = strcmp(model.rxnNames,'carbohydrate pseudoreaction');
-    for i = 1:length(model.mets)
-        S_ic   = model.S(i,cr_pos);
-        isProd = strcmp(model.metNames{i},'carbohydrate');
-        if S_ic ~= 0 && ~isProd
-            model.S(i,cr_pos) = fC*S_ic;
-        end
-    end
+    model = rescalePseudoReaction(model,'protein',fP);
+    model = rescalePseudoReaction(model,'carbohydrate',fC);
+    model = rescalePseudoReaction(model,'lipid backbone',fL);
+    model = rescalePseudoReaction(model,'lipid chain',fL);
 end
 
 %Change GAM:
@@ -52,6 +44,22 @@ for i = 1:length(model.mets)
     if S_ix ~= 0 && isGAM
         %Polymerization costs from Förster et al 2003 - table S8:
         model.S(i,xr_pos) = sign(S_ix)*(GAM + 37.7*Ptot + 12.8*Ctot);
+    end
+end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function model = rescalePseudoReaction(model,metName,f)
+
+rxnName = [metName ' pseudoreaction'];
+rxnPos  = strcmp(model.rxnNames,rxnName);
+for i = 1:length(model.mets)
+    S_ir   = model.S(i,rxnPos);
+    isProd = strcmp(model.metNames{i},metName);
+    if S_ir ~= 0 && ~isProd
+        model.S(i,rxnPos) = f*S_ir;
     end
 end
 
