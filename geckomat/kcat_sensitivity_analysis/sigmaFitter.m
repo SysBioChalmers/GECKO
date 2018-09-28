b3eab03f203542f4eb51badd8faf1a6d2ae9119d
Ivan Domenzain <ivand@chalmers.se>
2018-09-28 10:58:02 +0200

    fix: sigmaFitter algorithm optimization

--- geckomat/kcat_sensitivity_analysis/sigmaFitter.m
+++ geckomat/kcat_sensitivity_analysis/sigmaFitter.m
@@ -1,9 +1,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function OptSigma = sigmaFitter(model,Ptot,gR_exp,f,GAM)
+% function OptSigma = sigmaFitter(model,Ptot,expVal,f,GAM)
 % 
-% Function that recieves an EC model then finds the top limiting Kcat values
-% and tries to relax them according to the available data in BRENDA (Kcats
-% and SA*Mw entries).
+% Function that fits the average enzyme saturation factor in an ecModel
+% according to a provided experimentally measured value for the objective
+% function (i.e. growth rate at specified conditions)
 %
 % INPUTS:
 %       model       An EC batch model with an initial sigma factor assigned
@@ -11,59 +11,44 @@
 %                   enhanceGEM.m script
 %       Ptot        Total protein amount in the model (Experimental)
 %                   [g/gDw]
-%       gR_exp      Experimental growth rate on glucose minimal media 
-%                   [g/gDw hr]
+%       expVal      Experimentally measured value for the objective function
 %       f           Estimated mass fraction of enzymes in model [g/g]
 %       GAM         Growth associated maintenance
 %
 % OUTPUTS:
 %       optSigma    The optimal sigma value obtained
 %
-% Ivan Domenzain        2018-06-11
 % Benjamin Sanchez      2018-08-10
+% Ivan Domenzain        2018-09-27
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function OptSigma = sigmaFitter(model,Ptot,gR_exp,f,GAM)
+function OptSigma = sigmaFitter(model_batch,Ptot,expVal,f)
 
-%Compute f if not provided:
-if nargin < 4
-    [f,~] = measureAbundance(ecModel.enzymes);
-end
-
-%Fit GAM if not provided:
-if nargin < 5
-    GAM = fitGAM(model);
-end
-
-gRate_sim = [];
-error     = [];
+objValues   = [];
+errors    = [];
 sigParam  = [];
-c_source  = 'D-glucose exchange (reversible)';
-[model,~] = changeMedia_batch(model,c_source,'Min');
-cd ../limit_proteins
-for i=40:60
-    % Constrains the ecModel with the current sigma factor
+poolIndex = find(strcmpi(model_batch.rxnNames,'prot_pool_exchange'));
+objPos    = find(model_batch.c);
+%Relax bounds for the objective function
+model_batch.lb(objPos) = 0;
+model_batch.ub(objPos) = 1000;
+
+for i=1:100
+    %Constrains the ecModel with the i-th sigma factor
     sigma = i/100;
-    %model_batch  = changeCultureMedia(model);
-    [model_batch,~,~] = constrainEnzymes(model,Ptot,sigma,f,GAM);
-    % Change to minimal glucose media
-    gR_pos            = find(strcmpi(model_batch.rxnNames,'growth'));
-    model_batch.c     = zeros(size(model_batch.c));
-    model_batch.c(gR_pos)  = 1;
-    solution               = solveLP(model_batch);
-    model_batch.lb(gR_pos) = 0.999*solution.x(gR_pos);
-    model_batch.ub(gR_pos) = solution.x(gR_pos);
-    solution               = solveLP(model_batch,1);
-    gRate_sim              = [gRate_sim; solution.x(gR_pos)];
-    error                  = [error; abs((gR_exp-solution.x(gR_pos))/gR_exp)*100];
-    sigParam               = [sigParam; sigma];
-    disp(sigma)
-    disp(((gR_exp-solution.x(gR_pos))/gR_exp)*100)
+    model_batch.ub(poolIndex) = sigma*Ptot*f; 
+    solution  = solveLP(model_batch,1);
+    objValues = [objValues; solution.x(objPos)];
+    error     = abs(((expVal-solution.x(objPos))/expVal)*100);
+    errors    = [errors; error];
+    error     = num2str(((expVal-solution.x(objPos))/expVal)*100);
+    sigParam  = [sigParam; sigma];
+    disp(['Fitting sigma factor: ' num2str(sigma) '   error: ' error '%'])
 end
-[~, minIndx] = min(error);
+[~, minIndx] = min(errors);
 OptSigma     = sigParam(minIndx);
 figure
-plot(sigParam,error,'LineWidth',5)
+plot(sigParam,errors,'LineWidth',5)
 title('Sigma fitting for growth on glucose minimal media')
 xlabel('Average enzyme saturation [-]')
 ylabel('Absolute relative error [%]')
