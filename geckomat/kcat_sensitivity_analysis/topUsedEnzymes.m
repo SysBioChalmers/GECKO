2de6f02809533f5e4ae0aad09e7df806c90e10c9
Eduard Kerkhoven <eduardk@chalmers.se>
2018-12-05 17:49:44 +0100

    feat: topUsedEnzymes various changes
    - doc: explanation of input parameters
    - refactor: only generate table from cell array near end of function, for speed improvement and getting rid of warnings when new data was added to the table
    - feat: optional parameters: `writeFile` and `n` for number of top used enzymes, and now supporting `T` as command line output
    - feat: output table now contains additional column specifying the condition, this changes the output from before this commit

--- geckomat/kcat_sensitivity_analysis/topUsedEnzymes.m
+++ geckomat/kcat_sensitivity_analysis/topUsedEnzymes.m
@@ -1,42 +1,58 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function T = topUsedEnzymes(fluxes,model,conditions,name)
+% function T = topUsedEnzymes(fluxes,model,conditions,name,writeFile,n)
 %
-% Function that gets an ecModel_batch and a matrix of pFBA solution vectors 
+% Function that gets an ecModel_batch and a matrix of pFBA solution vectors
 % and calculate the top ten enzyme usages for every solution (conditions)
 % in a mass-wise way. The results are written in the topUsedEnzymes.txt
 % file and stored in the container folder.
 %
-% Ivan Domenzain    Last edited. 2018-09-25
+%   fluxes      cell array with pFBA solution vectors
+%   model       ecGEM structure used to generate pFBA solution vectors
+%   conditions  cell array of strings, with names identifying each pFBA
+%               solution vector
+%   name        string with model abbreviation (opt, default 'ecModel')
+%   writeFile   logical, whether a file should be written at the location
+%               gecko/models/'name' (opt, default true)
+%   n           number of top used enzymes to be listed. (opt, default 10)
+%
+% Ivan Domenzain    Last edited 2018-09-25
+% Eduard Kerkhoven  Last edited 2018-12-05
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function topUsedEnzymes(fluxes,model,conditions,name)
-    Indexes    = [];
-    varNames   = {};
-    outputFile = table;
-    % Find the enzyme usage reactions
-    usages = find(~cellfun(@isempty,strfind(model.rxnNames,'prot_')));
-    usages = usages(1:end-1);
-    usages = fluxes(usages,:);
+function T = topUsedEnzymes(fluxes,model,conditions,name,writeFile,n)
 
-    for i=1:length(usages(1,:))
-        %Units conversion [mmol/gDwh] -> [g prot/gDwh]
-        enzUsage{i} = usages(:,i).*model.MWs;
-        %Get the mass fraction of the proteome for every protein in the mod
-        enzUsage{i} = enzUsage{i}/sum(enzUsage{i}); 
-        %Sort and keep the top ten used enzymes
-        [enzUsage{i},Indexes{i}] = sort( enzUsage{i},'descend');
-         enzNames{i} = model.enzymes(Indexes{i});
-         enzUsage{i} = (enzUsage{i}(1:10));
-         enzNames{i} = enzNames{i}(1:10);
-         for j=1:10
-             outputFile(j,(2*i)-1) = enzNames{i}(j); 
-             outputFile(j,(2*i))   = {enzUsage{i}(j)};
-             varNames              = [varNames conditions(i)];
-         end
-    end
-    %Write the top-ten used enzyme names and their percentage usages for
-    %every condition on the output file
-    outputFile = truncateValues(outputFile,1);
-    writetable(outputFile,['../../models/' name '/' name '_topUsedEnzymes.txt'])
+if nargin < 6
+    n = 10;
 end
+if nargin < 5
+    writeFile = true;
+end
+% Find the enzyme usage reactions
+usages = find(~cellfun(@isempty,strfind(model.rxnNames,'prot_')));
+usages = usages(1:end-1);
+usages = fluxes(usages,:);
+[~, nConds] = size(usages);
+outputFile = cell(n,3*nConds);
 
-
+for i=1:length(usages(1,:))
+    %Units conversion [mmol/gDwh] -> [g prot/gDwh]
+    enzUsage = usages(:,i).*model.MWs;
+    %Get the mass fraction of the proteome for every protein in the mod
+    enzUsage = enzUsage/sum(enzUsage);
+    %Sort and keep the top ten used enzymes
+    [enzUsage,Indexes] = sort(enzUsage,'descend');
+    enzNames = model.enzymes(Indexes);
+    enzUsage = enzUsage(1:n);
+    enzNames = enzNames(1:n);
+    outputFile(:,(3*i)-2)   = enzNames;
+    outputFile(:,(3*i)-1)   = num2cell(enzUsage);
+    outputFile(:,(3*i))     = conditions(i);
+end
+%Write the top-ten used enzyme names and their percentage usages for
+%every condition on the output file
+i=1:length(usages(1,:));
+i=(3*i)-1;
+outputFile = truncateValues(outputFile,i);
+if writeFile
+    writetable(cell2table(outputFile),['../../models/' name '/' name '_topUsedEnzymes.txt'])
+end
+end
