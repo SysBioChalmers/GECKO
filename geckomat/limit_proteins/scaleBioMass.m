82421ffb432c490c644a7109e25c524d6232865d
benjasanchez <bensan@chalmers.se>
2018-12-17 01:57:01 +0100

    fix: scaleBioMass
    Only change pseudoreaction if the model has it

--- geckomat/limit_proteins/scaleBioMass.m
+++ geckomat/limit_proteins/scaleBioMass.m
@@ -60,11 +60,13 @@ function model = rescalePseudoReaction(model,metName,f)
 
 rxnName = [metName ' pseudoreaction'];
 rxnPos  = strcmp(model.rxnNames,rxnName);
-for i = 1:length(model.mets)
-    S_ir   = model.S(i,rxnPos);
-    isProd = strcmp(model.metNames{i},metName);
-    if S_ir ~= 0 && ~isProd
-        model.S(i,rxnPos) = f*S_ir;
+if sum(rxnPos) == 1
+    for i = 1:length(model.mets)
+        S_ir   = model.S(i,rxnPos);
+        isProd = strcmp(model.metNames{i},metName);
+        if S_ir ~= 0 && ~isProd
+            model.S(i,rxnPos) = f*S_ir;
+        end
     end
 end
 
