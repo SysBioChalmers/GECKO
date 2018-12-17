08b794e50f29b0408f2b7a406bfc86153c57edeb
Eduard Kerkhoven <eduardk@chalmers.se>
2018-12-17 18:24:42 +0100

    fix: bug in manualModifications
    - dimensions of model.S are `mets` x `rxns`, not the other way around

--- geckomat/change_model/manualModifications.m
+++ geckomat/change_model/manualModifications.m
@@ -449,10 +449,10 @@ function model = otherChanges(model)
     %(2015-11-05)
     index = find(strcmpi('prot_P19414',model.mets));
     if ~isempty(index)
-        rxnIndxs = find(model.S(:,index));
+        rxnIndxs = find(model.S(index,:));
         if ~isempty(rxnIndxs)
             rxnIndxs = rxnIndxs(1:end-2);
-            model.S(rxnIndxs,index) = model.S(rxnIndxs,index)/2;
+            model.S(index,rxnIndxs) = model.S(index,rxnIndxs)/2;
         end
     end
 end
