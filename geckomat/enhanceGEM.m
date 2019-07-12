afa18eed169a8c6c106f8c52bdda46cecffb3bc0
Ivan Domenzain <ivand@chalmers.se>
2019-07-12 19:48:25 +0200

    refactor: model parameters extracted from getModelParameters
    - Documentation has been improved

--- geckomat/enhanceGEM.m
+++ geckomat/enhanceGEM.m
@@ -1,9 +1,29 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% [ecModel,ecModel_batch] = enhanceGEM(model,toolbox,name,version)
-%
-% Benjamin J. Sanchez & Ivan Domenzain. Last edited: 2018-10-25
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [ecModel,ecModel_batch] = enhanceGEM(model,toolbox,name,version)
+% enhanceGEM
+%
+%   Main function for running the GECKO pipeline. It returns an ecModel and
+%   its constrained version with an upper limit in the total protein pool
+%   usage pseudoreaction (ecModel_batch) with calibrated Kcat parameters that
+%   allow it to grow at a specified experimental growth rate.
+%
+%   model       a GEM MATLAB structure compatible with the COBRA or RAVEN
+%               toolbox.
+%   toolbox     string with the name of the prefered toolbox for model SBML
+%               export (COBRA or RAVEN)
+%   name        Desired name for the ecModel (opt, default '')
+%   version     version of the original GEM (opt, default '')
+%
+%
+%   ecModel        an ecModel MATLAB structure suitable for incorporation of   
+%                  proteomics data as individual enzyme usage constraints.
+%   ecModel_batch  an ecModel MATLAB structure with a global constraint on 
+%                  the total protein pool usage pseudoreaction,
+%                  proportional to the measured total protein content (Ptot)
+%
+%   Usage: [ecModel,ecModel_batch] = enhanceGEM(model,toolbox,name,version)
+%
+%   Ivan Domenzain. Last edited: 2019-07-12
+%
 
 if nargin < 3
     name    = '';
@@ -12,16 +32,14 @@ if nargin < 4
     version = '';
 end
 
-%Provide your organism scientific name
-org_name = 'saccharomyces cerevisiae';
-
 %Convert model to RAVEN for easier visualization later on:
 format short e
 if isfield(model,'rules')
     initCobraToolbox
     model = ravenCobraWrapper(model);
 end
-
+%Get model-specific parameters
+parameters = getModelParameters;
 %Remove blocked rxns + correct model.rev:
 cd change_model
 [model,name,version] = preprocessModel(model,name,version);
@@ -29,7 +47,7 @@ cd change_model
 %Retrieve kcats & MWs for each rxn in model:
 cd ../get_enzyme_data
 model_data = getEnzymeCodes(model);
-kcats      = matchKcats(model_data,org_name);
+kcats      = matchKcats(model_data,parameters.org_name);
 
 %Integrate enzymes in the model:
 cd ../change_model
@@ -37,12 +55,8 @@ ecModel                 = readKcatData(model_data,kcats);
 [ecModel,modifications] = manualModifications(ecModel);
 
 %Constrain model to batch conditions:
-sigma    = 0.5;      %Optimized for glucose
-Ptot     = 0.5;      %Assumed constant
-gR_exp   = 0.41;     %[g/gDw h] Max batch gRate on minimal glucose media
-c_source = 'D-glucose exchange (reversible)'; %Rxn name for the glucose uptake reaction
 cd ../limit_proteins
-[ecModel_batch,OptSigma] = getConstrainedModel(ecModel,c_source,sigma,Ptot,gR_exp,modifications,name);
+[ecModel_batch,OptSigma] = getConstrainedModel(ecModel,parameters,modifications,name);
 disp(['Sigma factor (fitted for growth on glucose): ' num2str(OptSigma)])
 
 %Save output models:
