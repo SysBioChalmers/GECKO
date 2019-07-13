93385f784f5d04512741204a22e5639be8cc1aa9
Ivan Domenzain <ivand@chalmers.se>
2019-07-13 23:41:55 +0200

    feat: added parameters to getModelParameters.m

--- geckomat/getModelParameters.m
+++ geckomat/getModelParameters.m
@@ -8,21 +8,20 @@ function parameters = getModelParameters
 %
 
 %Average enzyme saturation factor
-parameters.sigma    = 0.5;
+parameters.sigma       = 0.5;
 %Total protein content in the cell [g protein/gDw]
-parameters.Ptot     = 0.5;      %Assumed constant
+parameters.Ptot        = 0.5;      %Assumed constant
 %Minimum growth rate the model should grow at [1/h]
-parameters.gR_exp   = 0.41;     %[g/gDw h] 
+parameters.gR_exp      = 0.41;     %[g/gDw h] 
 %Provide your organism scientific name
-parameters.org_name = 'saccharomyces cerevisiae';
+parameters.org_name    = 'saccharomyces cerevisiae';
 %Provide your organism KEGG ID
-parameters.keggID   = 'sce';
-%The name of the exchange reaction that supplies the model with carbon
-%(rxnNames)
-parameters.c_source = 'D-glucose exchange (reversible)'; 
-%Rxn name for biomass pseudoreaction
-parameters.bioRxn   = '';
+parameters.keggID      = 'sce';
+%The name of the exchange reaction that supplies the model with carbon (rxnNames)
+parameters.c_source    = 'D-glucose exchange (reversible)'; 
+%Rxn Id for biomass pseudoreaction
+parameters.bioRxn      = 'r_4041';
 %Compartment name in which the added enzymes should be located
-parameters.enzyme_comp = '';
+parameters.enzyme_comp = 'cytoplasm';
 
 end
