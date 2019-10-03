d1ba22741fba4004546830609f3207ebc14b5f0c
Ivan Domenzain <ivand@chalmers.se>
2019-10-03 14:01:56 +0200

    fix: draw_prot_XXX rxns excluded from results file

--- geckomat/utilities/ecFSEOF/run_ecFSEOF.m
+++ geckomat/utilities/ecFSEOF/run_ecFSEOF.m
@@ -21,7 +21,7 @@ function results = run_ecFSEOF(model,rxnTarget,cSource,alphaLims,Nsteps,file1,fi
 %
 % Usage: run_ecFSEOF(ecModel,rxnTarget,cSource,alphaLims,Nsteps)
 %
-% Last modified.  Ivan Domenzain 2019-09-13
+% Last modified.  Ivan Domenzain 2019-09-27
 %
 
 if nargin < 7
@@ -30,29 +30,39 @@ if nargin < 7
 		file1 = [];
 	end
 end
-current       = pwd;
-results.model = model;
+current = pwd;
 %Define alpha vector for suboptimal enforced objective values
-alphaV        = alphaLims(1):((alphaLims(2)-alphaLims(1))/(Nsteps-1)):alphaLims(2);
+alphaV  = alphaLims(1):((alphaLims(2)-alphaLims(1))/(Nsteps-1)):alphaLims(2);
 %Standardize grRules and rxnGeneMat in model
 [grRules,rxnGeneMat] = standardizeGrRules(model,true);
 model.grRules        = grRules;
 model.rxnGeneMat     = rxnGeneMat;
 %run FSEOF analysis
-results.substrate = compare_substrate(model,rxnTarget,cSource,alphaV,1E-4,true);
+results = ecFlux_scanning(model,rxnTarget,cSource,alphaV,1E-4,true);
 %Create gene table:
-results.geneTable      = cell(length(results.substrate.genes),3);
-results.geneTable(:,1) = results.substrate.genes;
-results.geneTable(:,2) = results.substrate.geneNames;
-results.geneTable(:,3) = num2cell(results.substrate.k_genes);
+results.geneTable      = cell(length(results.genes),3);
+results.geneTable(:,1) = results.genes;
+results.geneTable(:,2) = results.geneNames;
+results.geneTable(:,3) = num2cell(results.k_genes);
+%Create rxns table (exclude enzyme usage reactions):
+enzIndxs               = find(~startsWith(results.rxns(:,1),'draw_prot_'));
+results.k_rxns         = results.k_rxns(enzIndxs);
+results.rxnsTable      = cell(length(results.k_rxns),5);
+results.rxnsTable(:,1) = results.rxns(enzIndxs,1);
+results.rxnsTable(:,2) = results.rxns(enzIndxs,2);
+results.rxnsTable(:,3) = num2cell(results.k_rxns);
+results.rxnsTable(:,4) = results.rxns(enzIndxs,3);
+results.rxnsTable(:,5) = results.rxns(enzIndxs,4);
 try
     if ~isempty(file1)
-    	T = cell2table(results.geneTable);
-    	writetable(T,'Delimiter','\t','QuoteStrings',false) 
+        varNames = {'gene_IDs' 'gene_names' 'K_score'}; 
+    	T        = cell2table(results.geneTable,'VariableNames',varNames);
+    	writetable(T,file1,'Delimiter','\t','QuoteStrings',false) 
     end
     if ~isempty(file2)
-    	T = cell2table(results.k_rxns);
-    	writetable(T,'Delimiter','\t','QuoteStrings',false) 
+        varNames = {'rxn_IDs' 'rxnNames' 'K_score' 'grRules' 'rxn_formula'};
+    	T        = cell2table(results.rxnsTable,'VariableNames',varNames);
+        writetable(T,file2,'Delimiter','\t','QuoteStrings',false) 
     end
 catch
     warning('Output files directory not found');
