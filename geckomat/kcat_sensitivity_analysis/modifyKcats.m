51e3c0f5e5e99969a3e8d7d4070b75f7f0575562
Ivan Domenzain <ivand@chalmers.se>
2020-10-05 21:07:14 +0200

    fix: avoid redundancy on creation of model name-specific subfolders

--- geckomat/kcat_sensitivity_analysis/modifyKcats.m
+++ geckomat/kcat_sensitivity_analysis/modifyKcats.m
@@ -69,7 +69,6 @@ if ~isempty(changes)
     changes = cell2table(changes,'VariableNames',varNamesTable);
     changes = truncateValues(changes,[7:10]);
     %Write results in a .txt for further exploration.
-    mkdir (['../../models/' name]);
     writetable(changes,['../../models/' name '/' name '_kcatModifications.txt']);
 else
     %If the model is not feasible then the analysis is performed in all the 
