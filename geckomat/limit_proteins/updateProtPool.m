364cb2e8a8b0f275dcb8db1513fcfeb3487e6a46
Ivan Domenzain <ivand@chalmers.se>
2021-02-24 15:37:01 +0100

    fix: implement suggestions

--- geckomat/limit_proteins/updateProtPool.m
+++ geckomat/limit_proteins/updateProtPool.m
@@ -2,12 +2,12 @@ function model  = updateProtPool(model,Ptot,Pmeas,f,sigma)
 %Calculate total mass of bounded enzymes
 if any(isnan(model.concs))
     poolIndex  = find(strcmpi(model.rxns,'prot_pool_exchange'));
-    difference = Ptot-Pmeas;
+    difference = (Ptot*f-Pmeas);
     tempModel  = model;
-    if (difference)>=0
+    if (difference)>0
         %remove all measured enzymes mass (including flexibilizations) from
         %remaining pool
-        newPool   = (Ptot*f-Pmeas)*sigma;
+        newPool   = difference*sigma;
         tempModel = setParam(tempModel,'ub',poolIndex,newPool);
         solution  = solveLP(tempModel);
         if ~isempty(solution.x)
@@ -16,7 +16,7 @@ if any(isnan(model.concs))
         else
             %Allow any saturation level
             sigma     = 1;
-            newPool   = (Ptot*f-Pmeas)*sigma;
+            newPool   = difference*sigma;
             tempModel = setParam(tempModel,'ub',poolIndex,newPool);
             %As bounds for CUR and growth have already been set, protein
             %pool minimization is set as an objective
@@ -38,9 +38,18 @@ if any(isnan(model.concs))
             end
             model.c = originalC;
         end
-    else
-        warning('The total measured protein mass exceeds the total protein content.')
+    elseif (difference)<0
+        warning('The total measured protein mass exceeds the total protein content')
+        warning('Returning an empty ecModel')
         model = [];
+    else
+         warning('The total measured protein mass is equal to the total protein content, therefore no protein pool is available for the unmeasured model enzymes')
+         warning('Setting UB = 0 for prot_pool_exchange reaction')
+         model = setParam(tempModel,'ub',poolIndex,0);
+         sol = solveLP(model);
+         if isempty(sol.x)
+             warning('The resulting ecModel is unfeasible under the imposed constraints')
+         end
     end
 end
 end
