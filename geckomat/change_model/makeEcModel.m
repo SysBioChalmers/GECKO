491e9edb76fed9b477e27818ba9782e46a16b058
Eduard Kerkhoven <eduardk@chalmers.se>
2022-07-05 00:35:08 +0200

    feat: model.ec one entry per rxn, not substrates

--- geckomat/change_model/makeEcModel.m
+++ geckomat/change_model/makeEcModel.m
@@ -29,22 +29,15 @@ function model = makeEcModel(model,geckoLight)
 %       (complex).
 %   6.  Sort identifiers (so that split reactions remain close to each
 %       other, not real function, just makes it tidier.
-%   7.  Make reduced S-matrix with selected small chemicals (ions etc.) and
-%       pairs of currency metabolites (ATP+ADP etc.) are removed, to retain
-%       only the 'main' substrates for each reaction. This does not replace
-%       the regular S-matrix, but is used to get a smaller list of
-%       potential kcat values.
-%   8.  Make empty model.ec structure, that will contain enzyme and kcat
-%       information. One entry per reaction-substrate combination, where
-%       isoenzymes have multiple entries [not in geckoLight]. This model.ec
-%       structure will later be populated with kcat values.
-%   9.  Add metabolite/enzyme information fields to model.ec structure.
-%       Here there is only one entry per enzyme or metabolite, and includes
-%       info such as MW, sequence, SMILES etc.
-%   10. Populate model.ec structure (from step 8) with information from
+%   7.  Make empty model.ec structure, that will contain enzyme and kcat
+%       information. One entry per reaction, where isoenzymes have multiple
+%       entries [not in geckoLight]. This model.ec structure will later be
+%       populated with kcat values.
+%   8.  Add enzyme information fields to model.ec structure: MW, sequence.
+%   9.  Populate model.ec structure (from step 8) with information from
 %       each reaction.
-%   11. [Skipped with geckoLight:] Add proteins as pseudometabolites.
-%   12. Add prot_pool pseudometabolite.
+%   10. [Skipped with geckoLight:] Add proteins as pseudometabolites.
+%   11. Add prot_pool pseudometabolite.
 %   12. [Skipped with geckoLight:] Add draw reactions for the protein
 %       pseudometabolites.
 %   13. Add protein pool reaction, without upper bound.
@@ -111,43 +104,12 @@ end
 %6: Sort reactions, so that reversible and isoenzymic reactions are kept near
 model=sortIdentifiers(model);
 
-%7: Make a reduced S-matrix, with zero entries for metabolites to be
-%ignored for defining Kcat values.
-%TODO: have this loaded by a separate function, so that these lists can be
-%set as input parameters to the current function, avoiding repeated loading
-%of these files if multiple models are built.
-reducedS    = model.S;
-%Remove simple substrates
-fid         = fopen(fullfile(geckoPath,'databases','smallMets.tsv'));
-fileContent = textscan(fid,'%q','Delimiter','\t');
-fclose(fid);
-metsToIgnore = contains(model.metNames,fileContent{1});
-reducedS(metsToIgnore,:) = 0;
-
-%Remove currency metabolites
-fid         = fopen(fullfile(geckoPath,'databases','currencyMets.tsv'));
-fileContent = textscan(fid,'%q %q','Delimiter','\t');
-fclose(fid);
-currMets.sub = fileContent{1};
-currMets.pro = fileContent{2};
-
-%Reaction needs to involve both metabolites from a pair (=row in TSV file)
-for i=1:numel(currMets.sub)
-    idS = contains(model.metNames,currMets.sub{i});
-    idP = contains(model.metNames,currMets.pro{i});
-    [~,hasS] = find(reducedS(idS,:));
-    [~,hasP] = find(reducedS(idP,:));
-    hasBoth  = intersect(hasS,hasP);
-    reducedS([idS,idP],hasBoth) = 0;
-end
-
-%8: Make ec-extension structure, one for each kcat value
+%7: Make ec-extension structure, one for each kcat value
 emptyCell    = cell(numel(model.rxns)*5,1); %Prepare plenty of space
 emptyVect    = zeros(numel(model.rxns)*5,1);
 ec.rxns      = emptyCell;
 ec.eccodes   = emptyCell; % Can also be parsed from model. Only needed for classic GECKO matching
 ec.rxnEnzMat = zeros(numel(model.rxns)*5,numel(model.genes)); % Non-zeros will indicate the number of subunits
-%ec.substrate = emptyCell; 
 %ec.reverse   = logical(emptyVect); % Whether reaction is in reverse. Might be unnecessary, can also be gathered from the reaction ID ('_REV')
 ec.kcat      = emptyVect;
 ec.source    = emptyCell; % Strings, like 'dlkcat', 'manual', 'kcatdb'
@@ -156,9 +118,8 @@ ec.source    = emptyCell; % Strings, like 'dlkcat', 'manual', 'kcatdb'
 % number having a dedicated meaning), to avoid spelling mistakes and users
 % defining other types of sources?
 ec.notes     = emptyCell;
-%ec.inModel   = logical(emptyVect); which kcat value is actually used
 
-%9: Gather enzyme information via UniprotDB
+%8: Gather enzyme information via UniprotDB
 [Lia,Locb]      = ismember(model.genes,uniprotDB.genes);
 ec.genes        = model.genes(Lia); %Will often be duplicate of model.genes, but is done here to prevent issues when it is not.
 ec.enzymes      = uniprotDB.ID(Locb);
@@ -173,12 +134,6 @@ ec.concs        = nan(numel(ec.genes),1); % To be filled with proteomics data wh
 %     uniprotDB = extractMiriam(model.geneMiriams,'uniprot');
 % end
 
-%Gather substrate/metabolite information
-%ec.metNames  = unique(model.metNames);
-%ec.metSmiles = cell(numel(ec.metNames),1); % Could also be in model.metSmiles
-%TODO: include a metSmiles field in RAVEN, stored as annotation note in the SBML file
-%TODO: loading of external SMILES database for automated matching
-%TODO: option to extract SMILES from the model
 
 %Extract model provided eccodes
 %TODO: loading of external ec-code database, possibly from uniprot, which
@@ -189,35 +144,21 @@ ec.concs        = nan(numel(ec.genes),1); % To be filled with proteomics data wh
 %was dealt with there
 %ec.eccodes      = uniprotDB.eccodes(Locb);
 
-%10: Only parse rxns associated to genes
+%9: Only parse rxns associated to genes
 rxnWithGene = find(sum(model.rxnGeneMat,2));
 ecCount=1;
-for r=rxnWithGene' % model.rxns(r)
-    %Parse through substrates
-    substrates = find(reducedS(:,r)<0);
-    rxnGenes = model.genes(find(model.rxnGeneMat(r,:)));
-    [~,locEnz]=ismember(rxnGenes,ec.genes); % Could also parse directly from rxnGeneMat, but some genes might be missing from Uniprot DB
-    %Loop through all substrates
-    for s=substrates' % s = model.mets(s)
-        ec.rxns(ecCount)        = model.rxns(r);
-%         if ~isempty(regexp(model.rxns{r},'_REV($|_)'))
-%             ec.reverse(ecCount)	= true;
-%         else
-%             ec.reverse(ecCount) = false;
-%         end
-        ec.substrate(ecCount)   = model.metNames(s);
-        
-        %Fill in the gene information
-        ec.rxnEnzMat(ecCount,locEnz) = 1; %Assume 1 copy per subunit or enzyme, can be modified later
-        ecCount=ecCount+1;
-    end
+for r=1:numel(rxnWithGene)
+    ec.rxns(r) = model.rxns(rxnWithGene(r));
+    rxnGenes   = model.genes(find(model.rxnGeneMat(rxnWithGene(r),:)));
+    [~,locEnz] = ismember(rxnGenes,ec.genes); % Could also parse directly from rxnGeneMat, but some genes might be missing from Uniprot DB
+    ec.rxnEnzMat(r,locEnz) = 1; %Assume 1 copy per subunit or enzyme, can be modified later
 end
 %Remove empty fields
 for f=fieldnames(ec)'
     ec.(f{:})(ecCount-1:end,:)=[];
 end
 
-%11: Add proteins as pseudometabolites
+%10: Add proteins as pseudometabolites
 if ~geckoLight
     [proteinMets.mets, uniprotSortId] = sort(ec.enzymes);
     proteinMets.mets         = strcat('prot_',proteinMets.mets);
@@ -227,7 +168,7 @@ if ~geckoLight
     proteinMets.metNotes     = repmat({'Enzyme-usage pseudometabolite'},numel(proteinMets.mets),1);
     model = addMets(model,proteinMets);
 end
-%Add protein pool pseudometabolite
+%11: Add protein pool pseudometabolite
 pool.mets         = 'prot_pool';
 pool.metNames     = pool.mets;
 pool.compartments = 'c';
@@ -250,7 +191,7 @@ if ~geckoLight
     model = addRxns(model,drawRxns);
 end
 
-%Add protein pool reaction (with open UB)
+%13: Add protein pool reaction (with open UB)
 poolRxn.rxns            = 'prot_pool_exchange';
 poolRxn.mets            = {'prot_pool'};
 poolRxn.stoichCoeffs    = {-1};
