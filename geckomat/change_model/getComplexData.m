4249e7810a5d45aac4f2a62f4b251d30cea1c28f
edkerk <eduardk@chalmers.se>
2022-12-16 21:08:28 +0100

    feat: getComplexData "defined" column

--- geckomat/change_model/getComplexData.m
+++ geckomat/change_model/getComplexData.m
@@ -1,25 +1,39 @@
 function complexInfo = getComplexData(organism, writeFile)
 % getComplexData
+%   Download curated complex stochiometries from the EMBL-EBI Complex
+%   Portal database.
 %
-%   Download portal complex database with curated stochiomtery
-%   
-%   Input
-%       - organism: organism available in complex portal. e.g.
-%           * 'all'. For all the data in complex portal
-%           * 'Homo sapiens'
-%           * 'Mus musculus'
-%           * 'Saccharomyces cerevisiae'
-%         Note: If the organism is not defined, then download all
-%               available information.
+% Input:
+%   organism    the organism for which complex information should be
+%               downloaded, e.g.:
+%               - 'all' for all data in Complex Portal (default)
+%               - 'Homo sapiens'
+%               - 'Mus musculus'
+%               - 'Saccharomyces cerevisiae'
+%               See a list of all available organisms here:
+%               https://www.ebi.ac.uk/complexportal/complex/organisms
+%   outputFile  location of the output file, possible values:
+%               - true: write to GECKO/databases/complex_data.json
+%               - false: do not write output file (default)
+%               - any other path to where the complex_data.json file should
+%                 be written
 %
-%       - writeFile: define if save the data as JSON format file.
-%         default = false
-%
-%   Output
-%       - complexInfo: data downloaded from complex portal
-%
-%   Usage
-%         complexInfo = getComplexData('Saccharomyces cerevisiae',true)
+% Output:
+%   complexInfo structure with data downloaded from Complex Portal.
+%               Contains the following fields:
+%               - complexID: id of the complex on Complex Portal
+%               - name: name of the complex on Complex Portal
+%               - species: organism containing the complex
+%               - geneName: names of the genes in the complex
+%               - protID: Uniprot IDs of the proteins in the complex
+%               - stochiometry: the complex stochiometry given in the same
+%                 order as the genes and proteins in geneName and protID
+%               - defined: 0 if Complex Portal has no defined stochiometry
+%                          1 if defined subunit stochiometry is given
+%                          2 if complex consists of sub-complexes, whose
+%                            subunit stochiometries are given
+% Usage
+%   complexInfo = getComplexData('Saccharomyces cerevisiae','complex_data.json')
 
 if nargin<1
     organism = 'all';
@@ -53,6 +67,7 @@ switch writeFile
         writeFile = true;
 end
 
+weboptions.Timeout = 20;
 try
     url1 = 'https://www.ebi.ac.uk/intact/complex-ws/search/*';
     if ~isempty(organism)
@@ -60,14 +75,13 @@ try
     end
 
     data = webread(url1);
-
 catch ME
     if (strcmp(ME.identifier,'MATLAB:webservices:HTTP404StatusCodeError'))
-        error('Cannot connect to the complex portal, perhaps the server is not responding');
+        error('Cannot connect to the Complex Portal, perhaps the server is not responding');
     end
 end
 
-complexData = cell(data.size,6);
+complexData = cell(data.size,7);
 for i = 1:data.size
 
     url2 = 'https://www.ebi.ac.uk/intact/complex-ws/complex/';
@@ -84,55 +98,84 @@ for i = 1:data.size
         temp = [];
     end
 
-    if ~isempty(temp)
-        complexData(i,1) = {temp.complexAc};
-        complexData(i,2) = {temp.name};
-        complexData(i,3) = {temp.species};
 
-        idxIntType = find(strcmpi({temp.participants.interactorType}, 'protein'));
-        
-        % Some complex reported are 'stable complex', then, save the id
-        % complex and but set the genes and protein to a empty string.
-        if numel(idxIntType) > 0 
-            complexData(i,4) = {{temp.participants(idxIntType).name}};
-            complexData(i,5) = {{temp.participants(idxIntType).identifier}};
-        else
-            complexData(i,4) = {' '};
-            complexData(i,5) = {' '};
-        end
+if ~isempty(temp)
+    complexData(i,1) = {temp.complexAc};
+    complexData(i,2) = {temp.name};
+    complexData(i,3) = {temp.species};
 
-        % Portal complex has two stochiometry values, a minimum and
-        % maximum value. Only minimum will be store. In some cases,
-        % some complex does not have stochiometry coefficient, then, it
-        % will be fill with zeros
-        if ~cellfun('isempty',{temp.participants.stochiometry})
-            % For some reason, if there is only one protein in the complex
-            % split function does nor return a cell nx2, instead is 2x1,
-            % then assign an incorrect stochiometry 
-            if numel(idxIntType) > 1
-                stochiometry = split({temp.participants(idxIntType).stochiometry}.', ',');
-            else
-                stochiometry = split({temp.participants(idxIntType).stochiometry}.', ',').';
-            end 
-            values = str2double(erase(stochiometry(:,1),"minValue: ")).';
-            complexData(i,6) = {values};
-        else
-            complexData(i,6) = {repelem(0,numel(complexData{i,4}))};
-        end
+    idxIntType = find(strcmpi({temp.participants.interactorType}, 'protein'));
+
+    % Some complex reported are 'stable complex', then, save the id
+    % complex and but set the genes and protein to a empty string.
+    if numel(idxIntType) > 0
+        complexData(i,4) = {{temp.participants(idxIntType).name}};
+        complexData(i,5) = {{temp.participants(idxIntType).identifier}};
     else
-        %
-        complexData(i,1) = {complexID};
-        complexData(i,2) = {' '};
-        complexData(i,3) = {' '};
-        complexData(i,4) = {' '};
-        complexData(i,5) = {' '};
-        complexData(i,6) = {0};
+        complexData(i,4) = {{temp.participants.name}};
+        complexData(i,5) = {{temp.participants.identifier}};
+    end
+
+    % Portal complex has two stochiometry values, a minimum and
+    % maximum value. Only minimum will be store. In some cases,
+    % some complex does not have stochiometry coefficient, then, it
+    % will be fill with zeros
+    if ~cellfun('isempty',{temp.participants.stochiometry})
+        % For some reason, if there is only one protein in the complex
+        % split function does nor return a cell nx2, instead is 2x1,
+        % then assign an incorrect stochiometry
+        switch numel(idxIntType)
+            case 0 % Contains complexes
+                stochiometry = split({temp.participants.stochiometry}.', ',');
+                complexData(i,7) = {2};
+            case 1 % Contains one protein
+                stochiometry = split({temp.participants(idxIntType).stochiometry}.', ',').';
+                complexData(i,7) = {1};
+            otherwise
+                stochiometry = split({temp.participants(idxIntType).stochiometry}.', ',');
+                complexData(i,7) = {1};
+        end
+        values = str2double(erase(stochiometry(:,1),"minValue: ")).';
+        complexData(i,6) = {values};
+    else
+        complexData(i,6) = {repelem(0,numel(complexData{i,4}))};
+        complexData(i,7) = {0};
+    end
+else
+    %
+    complexData(i,1) = {complexID};
+    complexData(i,2) = {' '};
+    complexData(i,3) = {' '};
+    complexData(i,4) = {' '};
+    complexData(i,5) = {' '};
+    complexData(i,6) = {0};
+    complexData(i,7) = {0};
+end
+end
+% Expand complexes of complexes
+complexComplex = [complexData{:,7}]==2;
+if ~isempty(complexComplex)
+    for i=1:numel(complexComplex)
+        subComplex    = complexData{complexComplex(i),5};
+        subComplexS   = complexData{complexComplex(i),6};
+        subComplexIdx = find(ismember(complexData(:,1),subComplex));
+        allGenes = horzcat(complexData{subComplexIdx,4});
+        allProts = horzcat(complexData{subComplexIdx,5});
+        allStoch = {complexData{subComplexIdx,6}};
+        for j=1:numel(subComplex)
+            allStoch{j}=allStoch{j}*subComplexS(j);
+        end
+        allStoch = horzcat(allStoch{:});
+        [allGenes,ia,ic] = unique(allGenes,'stable');
+        allProts = allProts(ia);
+        allStoch = splitapply(@sum, allStoch', ic);
+        complexData{complexComplex(i),4} = allGenes;
+        complexData{complexComplex(i),5} = allProts;
+        complexData{complexComplex(i),6} = allStoch;
     end
 end
 
-complexData = sortrows(complexData,3);
-
-rowHeadings = {'complexID','name','specie','geneName','protID','stochiometry'};
+rowHeadings = {'complexID','name','species','geneName','protID','stochiometry','defined'};
 
 complexInfo = cell2struct(complexData, rowHeadings, 2);
 
