80793d8e90cfae433f758e9d10e806d6234e4527
edkerk <eduardk@chalmers.se>
2022-12-21 17:18:57 +0100

    refactor: loadBRENDAdata find database files

--- geckomat/get_enzyme_data/loadBRENDAdata.m
+++ geckomat/get_enzyme_data/loadBRENDAdata.m
@@ -1,15 +1,16 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- function [KCATcell, SAcell] = loadBRENDAdata
-     KCAT_file      = '../../databases/max_KCAT.txt';
-     SA_file        = '../../databases/max_SA.txt';
-     MW_file        = '../../databases/max_MW.txt';
+ function [KCATcell, SAcell] = loadBRENDAdata 
+     % TODO: potential refactor with DLKcat derived BRENDA data
+     geckoPath      = findGECKOroot;
+     KCAT_file      = fullfile(geckoPath,'databases','max_KCAT.txt');
+     SA_file        = fullfile(geckoPath,'databases','max_SA.txt');
+     MW_file        = fullfile(geckoPath,'databases','max_MW.txt');
+    
      %Extract BRENDA DATA from files information
-     scallingFactor = 3600;   %[1/s] -> [1/h]
-     KCATcell       = openDataFile(KCAT_file,scallingFactor); 
-     scallingFactor = 60;     %[umol/min/mg] -> [mmol/h/g]
-     SA             = openDataFile(SA_file,scallingFactor); 
-     scallingFactor = 1/1000; %[g/mol] -> [g/mmol]
-     MW             = openDataFile(MW_file,scallingFactor); 
+     scalingFactor = 1/60;    %[umol/min/mg] -> [mmol/s/g]    Old: 60 [umol/min/mg] -> [mmol/h/g]
+     SA            = openDataFile(SA_file,scalingFactor);
+     scalingFactor = 1/1000;  %[g/mol] -> [g/mmol]
+     MW            = openDataFile(MW_file,scalingFactor);
      
      for i=1:4
          SAcell{i} = [];
@@ -51,18 +52,27 @@
          end
          previousEC = SA{1}(i);
      end
+     
+     %remove EC in front of all the EC numbers
+     if ~isempty(KCATcell{1})
+         KCATcell{1} = extractAfter(KCATcell{1},2);
+     end
+     if ~isempty(SAcell{1})
+        SAcell{1} = extractAfter(SAcell{1},2);
+     end
+
  end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  function string_cells = stringSplit(cell_array)
-         string_cells = {strsplit(cell_array,'//')};
+         string_cells = {textscan(cell_array,'%q','Delimiter','//')};
          string_cells = string_cells{1}(1);
  end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- function data_cell = openDataFile(fileName,scallingFactor)
+ function data_cell = openDataFile(fileName,scalingFactor)
      fID          = fopen(fileName);
      data_cell    = textscan(fID,'%s %s %s %f  %s','delimiter','\t');
      fclose(fID);
-     data_cell{4} = data_cell{4}*scallingFactor;
+     data_cell{4} = data_cell{4}*scalingFactor;
      %Split string for each organism in the BRENDA data 
      %{name, taxonomy, KEGG code}
      data_cell{3}  = cellfun(@stringSplit, data_cell{3});
