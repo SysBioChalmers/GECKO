e5f38b779752ab7ac807e28a52b76071d000ead5
johan-gson <jgustafs@broadinstitute.org>
2023-01-22 20:51:41 -0500

    fix: findMetSmiles was unreliable, the web calls sometimes failed which lead to that it was not deterministic and produced different results every time it was run. A loop was added that runs it again if it fails for a metabolite.

--- src/geckomat/change_model/findMetSmiles.m
+++ src/geckomat/change_model/findMetSmiles.m
@@ -21,12 +21,26 @@ for i = 1:numel(uniqueNames)
         progress = pad(progress,3,'left');
         fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b%s%% complete',progress);
     end
-        try
-            smileResult       = webread(['https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/' uniqueNames{i} '/property/CanonicalSMILES/TXT']);
-            %Sometimes multiple lines are given, with alternative SMILES. Only
-            %keep the first suggestion.
-            smileResult       = regexp(smileResult,'(^\S*)\n','once','tokens');
-            uniqueSmiles{i,1} = smileResult{1,1};
+        retry = true;
+        while retry
+            try
+                retry = false;
+                smileResult       = webread(['https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/' uniqueNames{i} '/property/CanonicalSMILES/TXT']);
+                %Sometimes multiple lines are given, with alternative SMILES. Only
+                %keep the first suggestion.
+                smileResult       = regexp(smileResult,'(^\S*)\n','once','tokens');
+                uniqueSmiles{i,1} = smileResult{1,1};
+            catch exception
+                %Sometimes the call fails, for example since the server is busy. In those cases
+                %we should retry until we get a response. Some errors however are because the metabolite
+                %name doesn't exist in the database (404) or some other error (the metabolite contains
+                %a slash or similar, 400) - in those cases we need to give up, otherwise the function
+                %will enter an infinite loop.
+                if ~(strcmp(exception.identifier, 'MATLAB:webservices:HTTP404StatusCodeError') || ...
+                     strcmp(exception.identifier, 'MATLAB:webservices:HTTP400StatusCodeError'))
+                    retry = true;
+                end
+            end
         end
 end
 fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\bdone.\n');
