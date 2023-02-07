0b1c8cee29145030964584a8b6641fafb8bd1a46
edkerk <eduardk@chalmers.se>
2023-02-07 01:09:27 +0100

    feat: findMetSmiles stores model-specific SMILES

--- src/geckomat/change_model/findMetSmiles.m
+++ src/geckomat/change_model/findMetSmiles.m
@@ -1,4 +1,4 @@
-function metSmiles = findMetSmiles(metNames)
+function metSmiles = findMetSmiles(metNames, modelAdapter)
 % findMetSMILES
 %   Queries PubChem by metabolite names to obtain SMILES.
 %
@@ -8,19 +8,47 @@ function metSmiles = findMetSmiles(metNames)
 % Ouput:
 %   metSmiles   array with matching SMILES. If model.metNames is used as
 %               input, metSmiles can be specified as model.metSmiles.
-% 
+%
 
-[uniqueNames, ~, uniqueIdx]          = unique(metNames);
-uniqueSmiles(1:numel(uniqueNames),1) = {''};
-
-fprintf('Querying PubChem for SMILES by metabolite names...   0%% complete');
-numUnique = numel(uniqueNames);
-for i = 1:numel(uniqueNames)
-    if rem(i-1,floor(numUnique/100+1)) == 0
-        progress = num2str(floor(100*(i/numUnique)));
-        progress = pad(progress,3,'left');
-        fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b%s%% complete',progress);
+if nargin < 2 || isempty(modelAdapter)
+    modelAdapter = ModelAdapterManager.getDefaultAdapter();
+    if isempty(modelAdapter)
+        error('Either send in a modelAdapter or set the default model adapter in the ModelAdapterManager.')
     end
+end
+params = modelAdapter.params;
+
+[uniqueNames, ~, uniqueIdx] = unique(metNames);
+uniqueSmiles(1:numel(uniqueNames),1) = {''};
+protMets = startsWith(uniqueNames,'prot_');
+
+fprintf('Check for local SMILES database... ')
+smilesDBfile = (fullfile(params.path,'data','smilesDB.tsv'));
+if exist(smilesDBfile,'file')
+    fID = fopen(smilesDBfile,'r');
+    raw = textscan(fID,'%s %s','Delimiter','\t','HeaderLines',0);
+    fclose(fID);
+    smilesDB.names = raw{1};
+    smilesDB.smile = raw{2};
+    [metMatch, metIdx] = ismember(uniqueNames,smilesDB.names);
+    uniqueSmiles(metMatch) = smilesDB.smile(metIdx(metMatch));
+    fprintf('done.\n')
+else
+    fprintf('not found.\n')
+end
+
+if any(~metMatch & ~protMets)
+    fprintf('Querying PubChem for SMILES by metabolite names...   0%% complete');
+    numUnique = numel(uniqueNames);
+    for i = 1:numel(uniqueNames)
+        if metMatch(i) || protMets(i)
+            break;
+        end
+        if rem(i-1,floor(numUnique/100+1)) == 0
+            progress = num2str(floor(100*(i/numUnique)));
+            progress = pad(progress,3,'left');
+            fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b%s%% complete',progress);
+        end
         retry = true;
         while retry
             try
@@ -37,13 +65,18 @@ for i = 1:numel(uniqueNames)
                 %a slash or similar, 400) - in those cases we need to give up, otherwise the function
                 %will enter an infinite loop.
                 if ~(strcmp(exception.identifier, 'MATLAB:webservices:HTTP404StatusCodeError') || ...
-                     strcmp(exception.identifier, 'MATLAB:webservices:HTTP400StatusCodeError'))
+                        strcmp(exception.identifier, 'MATLAB:webservices:HTTP400StatusCodeError'))
                     retry = true;
                 end
             end
         end
+    end
+    fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\bdone.\n');
 end
-fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\bdone.\n');
 
 metSmiles = uniqueSmiles(uniqueIdx);
+out = [uniqueNames(~protMets), uniqueSmiles(~protMets)]';
+fID = fopen(smilesDBfile,'w');
+fprintf(fID,'%s\t%s\n',out{:});
+fclose(fID);
 end
