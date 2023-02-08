9ff94d8c534ccb94008efbd0ebbaa76255ac17b5
edkerk <eduardk@chalmers.se>
2023-02-08 01:44:13 +0100

    feat: sensTune output kcat changes and custom FC

--- src/geckomat/kcat_sensitivity_analysis/sensitivityTuning.m
+++ src/geckomat/kcat_sensitivity_analysis/sensitivityTuning.m
@@ -1,4 +1,4 @@
-function model = sensitivityTuning(model, desiredGrowthRate, modelAdapter)
+function [model, tunedKcats] = sensitivityTuning(model, desiredGrowthRate, modelAdapter, foldChange)
 % sensitivityTuning
 %    Function that relaxes the most limiting kcats until a certain growth rate 
 %    is reached. The function will update kcats in model.ec.kcat.
@@ -8,26 +8,31 @@ function model = sensitivityTuning(model, desiredGrowthRate, modelAdapter)
 %   desiredGrowthRate  kcats will be relaxed until this growth rate is reached
 %   modelAdapter       a loaded model adapter (Optional, will otherwise use the
 %                      default model adapter).
+%   foldChange         kcat values will be increased by this fold-change.
+%                      (Opt, default 10)
 % Output:
 %   model              ecModel with updated model.ec.kcat
 %
 % Usage:
 %   model = sensitivityTuning(model, 0.07, humanGEMAdapter)
 
+if nargin < 4 || isempty(foldChange)
+    foldChange = 10;
+end
 if nargin < 3 || isempty(modelAdapter)
     modelAdapter = ModelAdapterManager.getDefaultAdapter();
     if isempty(modelAdapter)
         error('Either send in a modelAdapter or set the default model adapter in the ModelAdapterManager.')
     end
 end
-
 params = modelAdapter.params;
+if nargin < 2 || isempty(desiredGrowthRate)
+    desiredGrowthRate = params.gR_exp;
+end
 
-
-lastGrowth = 0;
-
+kcatList = [];
 m = model;
-m.c = double(strcmp(m.rxns, params.bioRxn));%biomass_human set objective function to growth
+m.c = double(strcmp(m.rxns, params.bioRxn));% Make sure that growth is maximized
 
 %To avoid numerical issues, make sure no kcat is below 0.1
 %This is not desirable to do, but unfortunately necessary - otherwise the 
@@ -37,7 +42,7 @@ m.c = double(strcmp(m.rxns, params.bioRxn));%biomass_human set objective functio
 
 if ~m.ec.geckoLight
     %for the full model, we first find the draw reaction with the most flux
-    drawRxns = startsWith(m.rxns, 'draw_prot_');
+    drawRxns = startsWith(m.rxns, 'usage_prot_');
     iteration = 1;
     while true
         res = solveLP(m,0); %skip parsimonius, only takes time
@@ -59,9 +64,10 @@ if ~m.ec.geckoLight
         %now find the reaction with the largest consumption of this protein
         protFluxes = m.S(metSel,:).' .* res.x; %negative
         [~,rxnSel] = min(protFluxes);
+        kcatList = [kcatList, rxnSel];
         rxn = m.rxns(rxnSel);
         targetSubRxn = strcmp(m.ec.rxns, rxn);
-        m.ec.kcat(targetSubRxn) = m.ec.kcat(targetSubRxn) .* 10;
+        m.ec.kcat(targetSubRxn) = m.ec.kcat(targetSubRxn) .* foldChange;
         m = applyKcatConstraints(m,targetSubRxn);
     end
     
@@ -82,12 +88,25 @@ else
         %find the highest protein usage flux
         protPoolStoich = m.S(strcmp(m.mets, 'prot_pool'),:).';
         [~,sel] = min(res.x .* protPoolStoich); %max consumption
+        kcatList = [kcatList, find(sel)];
         rxn = m.rxns(sel.');
         targetSubRxns = strcmp(origRxns, rxn);
-        m.ec.kcat(targetSubRxns) = m.ec.kcat(targetSubRxns) .* 10;
+        m.ec.kcat(targetSubRxns) = m.ec.kcat(targetSubRxns) .* foldChange;
         m = applyKcatConstraints(m,targetSubRxns);
     end
 end
+
+kcatList        = unique(kcatList);
+tunedKcats.rxns = m.rxns(kcatList);
+tunedKcats.rxnNames = m.rxnNames(kcatList);
+[~, rxnIdx]     = ismember(tunedKcats.rxns,m.ec.rxns);
+tunedKcats.enzymes = cell(numel(kcatList),1);
+for i=1:numel(rxnIdx)
+    [~, metIdx]     = find(m.ec.rxnEnzMat(rxnIdx(i),:));
+    tunedKcats.enzymes{i}   = strjoin(m.ec.enzymes(metIdx),';');
+end
+tunedKcats.oldKcat = model.ec.kcat(rxnIdx);
+tunedKcats.newKcat = m.ec.kcat(rxnIdx);
 
 model = m;
 
