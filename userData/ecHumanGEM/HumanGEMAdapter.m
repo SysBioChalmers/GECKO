9ed48d07620ce4e8321a4f9f151e8f5caaf03223
Mihail Anton <mihail.anton@chalmers.se>
2023-02-25 07:15:00 +0100

    docs: styling in adapters

--- userData/ecHumanGEM/HumanGEMAdapter.m
+++ userData/ecHumanGEM/HumanGEMAdapter.m
@@ -1,40 +1,40 @@
 classdef HumanGEMAdapter < ModelAdapter 
     methods
         function obj = HumanGEMAdapter()
-            %Set initial values of the parameters - they can be changed by the user
+            % Set initial values of the parameters - they can be changed by the user
             
-            %Directory where all model-specific files and scripts are kept.
-            %Is assumed to follow the GECKO-defined folder structure. The
-            %code below refers to userData/ecYeastGEM in the GECKO path.
+            % Directory where all model-specific files and scripts are kept.
+            % Is assumed to follow the GECKO-defined folder structure. The
+            % code below refers to userData/ecHumanGEM in the GECKO path.
             geckoPath = findGECKOroot;
             obj.params.path = fullfile(geckoPath,'userData','ecHumanGEM');
 
-			%Path to the conventional GEM that this ecModel will be based on.
+			% Path to the conventional GEM that this ecModel will be based on.
 			obj.params.convGEM = fullfile(HumanGEMAdapter.getHumanGEMRootPath(),'model','Human-GEM.xml');
 
-            %Average enzyme saturation factor
+            % Average enzyme saturation factor
 			obj.params.sigma = 0.1; %This was changed to a low number to give a reasonable growth rate - this should be investigated more
 
-			%Total protein content in the cell [g protein/gDw]
+			% Total protein content in the cell [g protein/gDw]
 			obj.params.Ptot = 0.505717472;  %Average across NCI60 cell lines
 
-			%Fraction of enzymes in the model [g enzyme/g protein]
+			% Fraction of enzymes in the model [g enzyme/g protein]
 			obj.params.f = 0.412; %estimated as TPM of model genes / all genes in GTEx
 
-			%Minimum growth rate the model should grow at [1/h]
+			% Minimum growth rate the model should grow at [1/h]
 			obj.params.gR_exp = 0.020663429; %[g/gDw h]/Average across NCI60 cell lines
             
-			%Experimental carbon source uptake (optional)
+			% Experimental carbon source uptake (optional)
 			obj.params.c_UptakeExp = 0.641339301; %[mmol/gDw h]/Average across NCI60 cell lines
             
-            %Provide your organism scientific name
+            % Provide your organism scientific name
 			obj.params.org_name = 'homo sapiens';
             
-            %Matching name for Complex Portal
+            % Matching name for Complex Portal
             obj.params.complex_org_name = 'Homo sapiens';
 
-			%Provide your organism KEGG ID, selected at
-			%https://www.genome.jp/kegg/catalog/org_list.html
+			% Provide your organism KEGG ID, selected at
+			% https://www.genome.jp/kegg/catalog/org_list.html
 			obj.params.keggID = 'hsa';
             % Field for KEGG gene identifier; should match the gene
             % identifiers used in the model. With 'kegg', it takes the
@@ -46,34 +46,34 @@ classdef HumanGEMAdapter < ModelAdapter
             % all organisms and/or genes.
             obj.params.keggGeneIdentifier = 'Ensembl';
 
-			%Provide what identifier should be used to query UniProt.
-            %Select proteome IDs at https://www.uniprot.org/proteomes/
-            %or taxonomy IDs at https://www.uniprot.org/taxonomy.
+			% Provide what identifier should be used to query UniProt.
+            % Select proteome IDs at https://www.uniprot.org/proteomes/
+            % or taxonomy IDs at https://www.uniprot.org/taxonomy.
             obj.params.uniprotIDtype = 'proteome'; % 'proteome' or 'taxonomy_id'
 			obj.params.uniprotID = 'UP000005640'; % should match the ID type
-            %Field for Uniprot gene id - should match the gene ids used in the 
+            % Field for Uniprot gene id - should match the gene ids used in the 
             %model. It should be one of the "Returned Field" entries under
-            %"Names & Taxonomy" at this page: https://www.uniprot.org/help/return_fields
-            %Human-GEM uses Ensemble IDs, but these are not easily retrievable
-            %from Uniprot. Instead, we will gather gene_primary entries, and in
-            %a separate function (getUniprotCompatibleGenes) are these matched
-            %to Ensembl IDs.
+            % "Names & Taxonomy" at this page: https://www.uniprot.org/help/return_fields
+            % Human-GEM uses Ensemble IDs, but these are not easily retrievable
+            % from Uniprot. Instead, we will gather gene_primary entries, and in
+            % a separate function (getUniprotCompatibleGenes) are these matched
+            % to Ensembl IDs.
             obj.params.uniprotGeneIdField = 'gene_primary';
-            %Whether only reviewed data from UniProt should be considered.
-            %Reviewed data has highest confidence, but coverage might be (very)
-            %low for non-model organisms
+            % Whether only reviewed data from UniProt should be considered.
+            % Reviewed data has highest confidence, but coverage might be (very)
+            % low for non-model organisms
             obj.params.uniprotReviewed = true;
             
-			%Reaction ID for glucose exchange reaction (or other preferred carbon source)
+			% Reaction ID for glucose exchange reaction (or other preferred carbon source)
 			obj.params.c_source = 'MAR09034'; 
 
-			%Reaction ID for biomass pseudoreaction
+			% Reaction ID for biomass pseudoreaction
 			obj.params.bioRxn = 'MAR13082';
 
-			%Reaction ID for non-growth associated maitenance pseudoreaction
+			% Reaction ID for non-growth associated maitenance pseudoreaction
 			obj.params.NGAM = 'MAR09931'; %This is not used
 
-			%Compartment name in which the added enzymes should be located
+			% Compartment name in which the added enzymes should be located
 			obj.params.enzyme_comp = 'Cytosol';
             
         end
@@ -84,27 +84,27 @@ classdef HumanGEMAdapter < ModelAdapter
 			spontRxnNames = rxns_tsv.rxns;
         end
 		
-        %Ensembl gene ids are not available in uniprot (ensembl returns transcripts, not genes)
-        %Therefore, we collect gene symbols from uniprot, and need to convert the ensembl genes
-        %here to gene symbols as well
+        % Ensembl gene ids are not available in uniprot (ensembl returns transcripts, not genes)
+        % Therefore, we collect gene symbols from uniprot, and need to convert the ensembl genes
+        % here to gene symbols as well
         function genes = getUniprotCompatibleGenes(obj,inGenes)
-            % get the path
+            % Get the path
             tmpfile = fullfile(HumanGEMAdapter.getHumanGEMRootPath(),'model','genes.tsv');
 
-            % import as structure, convert to table, and extract header
+            % Import as structure, convert to table, and extract header
             tmp = struct2table(importTsvFile(tmpfile));
             conv_key = table2array(tmp);
             clear tmp
 
-            %convert genes
+            % Convert genes
             genes = inGenes;
             [~,ia,ib] = intersect(genes, conv_key(:,1));
             genes(ia) = conv_key(ib,5);
             
-            %A special canse - GGT2 is not found in uniprot - however GGT2P is
-            %This is supposed to be a pseudogene, but I suspect the sequence is very similar, so let's use that
+            % A special canse - GGT2 is not found in uniprot - however GGT2P is
+            % This is supposed to be a pseudogene, but I suspect the sequence is very similar, so let's use that
             genes{strcmp(genes, 'GGT2')} = 'GGT2P';
-            %Also replace empty strings with something else to avoid matches on empty strings in uniprot
+            % Also replace empty strings with something else to avoid matches on empty strings in uniprot
             genes(strcmp(genes, '')) = {'<<<EMPTY>>>'};
         end
     end
@@ -116,7 +116,7 @@ classdef HumanGEMAdapter < ModelAdapter
             if ~endsWith(path, '/')
                 path = strcat(path,'/');
             end
-            %Now remove the model/ at the end
+            % Now remove the model/ at the end
             path = (path(1:strlength(path)-6));
         end
 	end
--- userData/yourModel/yourModelAdapter.m
+++ userData/yourModel/yourModelAdapter.m
@@ -1,38 +1,38 @@
 classdef yourModelAdapter < ModelAdapter
     methods
         function obj = yourModelAdapter()
-            %Set initial values of the obj.params - they can be changed by the user
+            % Set initial values of the obj.params - they can be changed by the user
             
-            %Directory where all model-specific files and scripts are kept.
-            %Is assumed to follow the GECKO-defined folder structure. The
-            %code below refers to userData/ecYeastGEM in the GECKO path.
+            % Directory where all model-specific files and scripts are kept.
+            % Is assumed to follow the GECKO-defined folder structure. The
+            % code below refers to userData/yourModel in the GECKO path.
             geckoPath = findGECKOroot;
             obj.params.path = fullfile(geckoPath,'userData','yourModel');
 
-			%Path to the conventional GEM that this ecModel will be based on.
+			% Path to the conventional GEM that this ecModel will be based on.
 			obj.params.convGEM = fullfile(obj.params.path,'models','yourModel.xml');
 
-			%Average enzyme saturation factor
+			% Average enzyme saturation factor
 			obj.params.sigma = 0.5;
 
-			%Total protein content in the cell [g protein/gDw]
+			% Total protein content in the cell [g protein/gDw]
 			obj.params.Ptot = 0.5;
 
-			%Fraction of enzymes in the model [g enzyme/g protein]
+			% Fraction of enzymes in the model [g enzyme/g protein]
 			obj.params.f = 0.5;
             
-            %Growth rate the model should be able to reach when not
-            %constraint by nutrient uptake (e.g. max growth rate) [1/h]
+            % Growth rate the model should be able to reach when not
+            % constraint by nutrient uptake (e.g. max growth rate) [1/h]
 			obj.params.gR_exp = 0.41;
 
-			%Provide your organism scientific name
+			% Provide your organism scientific name
 			obj.params.org_name = 'saccharomyces cerevisiae';
             
-            %Matching name for Complex Portal
+            % Matching name for Complex Portal
             obj.params.complex_org_name = 'Saccharomyces cerevisiae';
 
-			%Provide your organism KEGG ID, selected at
-			%https://www.genome.jp/kegg/catalog/org_list.html
+			% Provide your organism KEGG ID, selected at
+			% https://www.genome.jp/kegg/catalog/org_list.html
 			obj.params.keggID = 'sce';
             % Field for KEGG gene identifier; should match the gene
             % identifiers used in the model. With 'kegg', it takes the
@@ -44,68 +44,66 @@ classdef yourModelAdapter < ModelAdapter
             % all organisms and/or genes.
             obj.params.keggGeneIdentifier = 'kegg';
 
-			%Provide what identifier should be used to query UniProt.
-            %Select proteome IDs at https://www.uniprot.org/proteomes/
-            %or taxonomy IDs at https://www.uniprot.org/taxonomy.
+			% Provide what identifier should be used to query UniProt.
+            % Select proteome IDs at https://www.uniprot.org/proteomes/
+            % or taxonomy IDs at https://www.uniprot.org/taxonomy.
             obj.params.uniprotIDtype = 'taxonomy_id'; % 'proteome' or 'taxonomy_id'
 			obj.params.uniprotID = '559292'; % should match the ID type
-            %Field for Uniprot gene ID - should match the gene ids used in the 
-            %model. It should be one of the "Returned Field" entries under
-            %"Names & Taxonomy" at this page: https://www.uniprot.org/help/return_fields
+            % Field for Uniprot gene ID - should match the gene ids used in the 
+            % model. It should be one of the "Returned Field" entries under
+            % "Names & Taxonomy" at this page: https://www.uniprot.org/help/return_fields
             obj.params.uniprotGeneIdField = 'gene_oln';
-            %Whether only reviewed data from UniProt should be considered.
-            %Reviewed data has highest confidence, but coverage might be (very)
-            %low for non-model organisms
+            % Whether only reviewed data from UniProt should be considered.
+            % Reviewed data has highest confidence, but coverage might be (very)
+            % low for non-model organisms
             obj.params.uniprotReviewed = true;
 
-			%Reaction ID for glucose exchange reaction (or other preferred carbon source)
+			% Reaction ID for glucose exchange reaction (or other preferred carbon source)
 			obj.params.c_source = 'r_1714'; 
 
-			%Reaction ID for biomass pseudoreaction
+			% Reaction ID for biomass pseudoreaction
 			obj.params.bioRxn = 'r_4041';
 
-			%Reaction ID for non-growth associated maitenance pseudoreaction
+			% Reaction ID for non-growth associated maitenance pseudoreaction
 			obj.params.NGAM = 'r_4046';
 
-			%Compartment name in which the added enzymes should be located
+			% Compartment name in which the added enzymes should be located
 			obj.params.enzyme_comp = 'cytoplasm';
 
-			%Rxn names for the most common experimentally measured "exchange" fluxes
-			%For glucose and o2 uptakes add the substring: " (reversible)" at the end
-			%of the corresponding rxn name. This is due to the irreversible model
-			%nature of ecModels. NOTE: This parameter is only used by fitGAM.m, so if
-			%you do not use said function you don not need to define it.
+			% Rxn names for the most common experimentally measured "exchange" fluxes
+			% For glucose and o2 uptakes add the substring: " (reversible)" at the end
+			% of the corresponding rxn name. This is due to the irreversible model
+			% nature of ecModels. NOTE: This parameter is only used by fitGAM.m, so if
+			% you do not use said function you don not need to define it.
 			obj.params.exch_names{1} = 'growth';
 			obj.params.exch_names{2} = 'D-glucose exchange (reversible)';
 			obj.params.exch_names{3} = 'oxygen exchange (reversible)';
 			obj.params.exch_names{4} = 'carbon dioxide exchange';
 
-			%Biomass components pseudoreactions (proteins, carbs and lipids lumped
-			%pools). NOTE: This parameter is only used by scaleBioMass.m, so if you do
-			%not use said function you don not need to define it. (optional)
+			% Biomass components pseudoreactions (proteins, carbs and lipids lumped
+			% pools). NOTE: This parameter is only used by scaleBioMass.m, so if you do
+			% not use said function you don not need to define it. (optional)
 			obj.params.bio_comp{1} = 'protein';
 			obj.params.bio_comp{2} = 'carbohydrate';
 			obj.params.bio_comp{3} = 'lipid backbone';
 			obj.params.bio_comp{4} = 'lipid chain';
 
-			%Polymerization costs from Forster et al 2003 - table S8. NOTE: This
-			%parameter is only used by scaleBioMass.m, so if you do not use said
-			%function you don not need to define it. (optional)
-			obj.params.pol_cost(1) = 37.7; %Ptot 
-			obj.params.pol_cost(2) = 12.8; %Ctot
-			obj.params.pol_cost(3) = 26.0; %RNA 
-			obj.params.pol_cost(4) = 26.0; %DNA
+			% Polymerization costs from Forster et al 2003 - table S8. NOTE: This
+			% parameter is only used by scaleBioMass.m, so if you do not use said
+			% function you don not need to define it. (optional)
+			obj.params.pol_cost(1) = 37.7; % Ptot 
+			obj.params.pol_cost(2) = 12.8; % Ctot
+			obj.params.pol_cost(3) = 26.0; % RNA 
+			obj.params.pol_cost(4) = 26.0; % DNA
 
-			%Rxn IDs for reactions in the oxidative phosphorylation pathway (optional)
+			% Rxn IDs for reactions in the oxidative phosphorylation pathway (optional)
 			obj.params.oxPhos{1} = 'r_1021';
 			obj.params.oxPhos{2} = 'r_0439';
 			obj.params.oxPhos{3} = 'r_0438';
 			obj.params.oxPhos{4} = 'r_0226';
 
-
-			%TODO: I'm not sure why this param exists for Human-GEM but not for Yeast-GEM - investigate.
-			%Experimental carbon source uptake (optional)
-			%obj.params.c_UptakeExp = 0.641339301; %[mmol/gDw h]/Average across NCI60 cell lines
+			% Experimental carbon source uptake (optional)
+			% obj.params.c_UptakeExp = 0.641339301; %[mmol/gDw h]/Average across NCI60 cell lines
         end
 		
 		function [spont,spontRxnNames] = getSpontaneousReactions(obj,model)
