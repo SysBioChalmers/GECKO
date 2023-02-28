1e6f5101876ccc1d5f3088bc1f1efe1ee79fcca9
edkerk <eduardk@chalmers.se>
2023-02-28 17:57:51 +0100

    fix: loadProtData

--- src/geckomat/utilities/loadProtData.m
+++ src/geckomat/utilities/loadProtData.m
@@ -42,6 +42,9 @@ function protData = loadProtData(replPerCond, protDataFile, filterData, modelAda
 %                   abundances  matrix of proteomics data, where each
 %                               column contains mean abundances per
 %                               condition
+%
+% Usage:
+%   protData = loadProtData(replPerCond, protDataFile, filterData, modelAdapter, minVal, maxRSD, maxMissing, cutLowest, addStdevs)
 
 if nargin < 8 || isempty(addStdevs)
     addStdevs = 1;
@@ -137,11 +140,11 @@ else
         if i<numel(replPerCond)
             abundances = abundances(:,replPerCond(i)+1:end);
         end
-        filtAbund(:,1) = mean(condAbund,2,'omitnan');
+        filtAbund(:,i) = mean(condAbund,2,'omitnan');
     end
 end
 notAllNan = logical(sum(~isnan(filtAbund),2));
-protData.abundances = filtAbund(notAllNan);
+protData.abundances = filtAbund(notAllNan,:);
 protData.uniprotIDs = uniprotIDs(notAllNan);
 end
 
