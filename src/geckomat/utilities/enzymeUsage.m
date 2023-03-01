026de3f12b5414e34844a752a933fc28755da1fd
edkerk <eduardk@chalmers.se>
2023-03-01 01:23:51 +0100

    fix: enzymeUsage

--- src/geckomat/utilities/enzymeUsage.m
+++ src/geckomat/utilities/enzymeUsage.m
@@ -1,6 +1,7 @@
-function [absUsage,capUsage,UB,protID] = enzymeUsage(ecModel,fluxes,zero)
+function usageData = enzymeUsage(ecModel,fluxes,zero)
 % enzymeUsage
-%   Gives enzyme usages based on a provided flux distribution. It can give:
+%   Gives enzyme usages based on a provided flux distribution, as obtained
+%   from a full GECKO model. It can give:
 %   1)  absolute usage: the specific enzyme usage in ug/gDCW/h, which can
 %       be given for enzymes with- and without concentration information;
 %   2)  capacity usage: the ratio of available enzyme that is used, calcuted
@@ -9,41 +10,43 @@ function [absUsage,capUsage,UB,protID] = enzymeUsage(ecModel,fluxes,zero)
 %   3)  UB: the upper bound of each enzyme exchange reaction, which may not
 %       be the same as the enzyme concentration, if it has been
 %       flexibilized
-%   4)  protId: the protein identifiers for each enzyme (if the model has an
+%   4)  protID: the protein identifiers for each enzyme (if the model has an
 %       enzymes field than this order is used, otherwise it is given
 %       alphabetically.
 %
 %  Input:
-%   ecModel         a GECKO3 ecModel
-%   fluxes          vector of fluxes, for instance sol.x
-%   zero            logical whether also enzymes with zero absolute usage
-%                   should be included (Optional, default true)
+%   ecModel     a full GECKO3 ecModel
+%   fluxes      vector of fluxes, for instance sol.x
+%   zero        logical whether also enzymes with zero absolute usage
+%               should be included (Optional, default true)
 %
 %  Output:
-%   capUsage        vector of enzyme capacity usages
-%   absUsage        vector of absolute enzyme usages
-%   UB              vector of enzyme exchange reaction upper bounds
-%   protID          string array of protein IDs matching the other output
+%   usageData   structure with enzyme usage data
+%               capUsage    vector of enzyme capacity usages
+%               absUsage    vector of absolute enzyme usages
+%               UB          vector of enzyme exchange reaction upper bounds
+%               protID      string array of matching protein IDs
 %
-% Usage: [absUsage,capUsage,UB,protID] = enzymeUsage(ecModel,fluxes,zero)
+% Usage: usageData = enzymeUsage(ecModel,fluxes,zero)
 
 if nargin<3
     zero=true;
 end
-
-protID      = ecModel.ec.enzymes;
+if ecModel.ec.geckoLight
+    error('This function does not work on GECKO light models.')
+end
+usageData.protID      = ecModel.ec.enzymes;
 [~,rxnIdx] = ismember(strcat('usage_prot_',ecModel.ec.enzymes),ecModel.rxns);
-%[~,rxnIdx]  = ismember(ecModel.rxns,strcat('usage_prot_',ecModel.ec.enzymes));
 
-UB          = ecModel.ub(rxnIdx);
-absUsage    = fluxes(rxnIdx);
-capUsage    = absUsage./UB;
+usageData.UB          = ecModel.ub(rxnIdx);
+usageData.absUsage    = fluxes(rxnIdx);
+usageData.capUsage    = usageData.absUsage./usageData.UB;
 
 if ~zero
-    nonzero     = absUsage>0;
-    absUsage    = absUsage(nonzero);
-    capUsage    = capUsage(nonzero);
-    UB          = UB(nonzero);
-    protId      = protId(nonzero);
+    nonzero     = usageData.absUsage>0;
+    usageData.absUsage    = usageData.absUsage(nonzero);
+    usageData.capUsage    = usageData.capUsage(nonzero);
+    usageData.UB          = usageData.UB(nonzero);
+    usageData.protID      = usageData.protID(nonzero);
 end
 end
