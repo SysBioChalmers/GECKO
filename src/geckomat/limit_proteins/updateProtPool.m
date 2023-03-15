36286e53b9c7492dd03b437d866b227f706c75fa
Albert Tafur Rangel <39610893+ae-tafur@users.noreply.github.com>
2023-03-15 13:05:08 +0100

    fix: protein pool in proteomics integration

--- src/geckomat/limit_proteins/updateProtPool.m
+++ src/geckomat/limit_proteins/updateProtPool.m
@@ -1,10 +1,11 @@
-function [model, newPtot]  = updateProtPool(model, Ptot, modelAdapter)
+function [model, newPtot]  = updateProtPool(model, measuredProt, Ptot, modelAdapter)
 % updateProtPool
 %   Updates the protein pool to compensate for measured proteomics data (in
 %   model.ec.concs).
 %
 % Input:
 %   model           an ecModel in GECKO 3 format (with ecModel.ec structure)
+%   measuredProt    average sum of total protein in proteomics.tsv in mg/gDW
 %   Ptot            total protein content, overwrites modelAdapter value
 %   modelAdapter    a loaded model adapter (Optional,
 %
@@ -13,9 +14,9 @@ function [model, newPtot]  = updateProtPool(model, Ptot, modelAdapter)
 %                   protein concentrations
 %   newPtot         new Ptot
 % Usage:
-%   [model, newPtot]  = updateProtPool(model, Ptot, modelAdapter)
+%   [model, newPtot]  = updateProtPool(model, measuredProt, Ptot, modelAdapter)
 
-if nargin < 3 || isempty(modelAdapter)
+if nargin < 4 || isempty(modelAdapter)
     modelAdapter = ModelAdapterManager.getDefaultAdapter();
     if isempty(modelAdapter)
         error('Either send in a modelAdapter or set the default model adapter in the ModelAdapterManager.')
@@ -23,22 +24,40 @@ if nargin < 3 || isempty(modelAdapter)
 end
 params = modelAdapter.params;
 
-if nargin < 2 || isempty(Ptot)
+if nargin < 3 || isempty(Ptot)
     Ptot = params.Ptot;
     disp(['Total protein content used: ' num2str(Ptot) ' [g protein/gDw]'])
 end
 
+if nargin < 2 || isempty(measuredProt)
+    % assumes not measured protein
+    measuredProt = Ptot * params.f * 1000;
+    disp(['Measured protein assumed: ' num2str(measuredProt/1000) ' [g protein/gDw]'])
+end
+
 originalLB = model.lb(strcmp(model.rxns,'prot_pool_exchange'));
-PmeasEnz = sum(model.ec.concs,'omitnan');
-params.f = PmeasEnz / (Ptot * 1000); % Update f, as the fraction of proteins with conc in the model
+% Convert ptot to mg/gDW
 PtotEnz = Ptot * 1000;
-PdiffEnz = PtotEnz - PmeasEnz;
+
+% New f factor
+f = measuredProt / PtotEnz;
+
+% Calculate the protein in the model
+PmeasEnz = sum(model.ec.concs,'omitnan');
+
+if PmeasEnz == 0
+    error('The model have not been constrained with proteomics')
+end
+
+% Draw the protein in model from the measured proteins
+PdiffEnz = measuredProt - PmeasEnz;
+
 if PdiffEnz > 0
     Pdiff = PdiffEnz / 1000; % Convert back to g protein/gDCW
-    model = setProtPoolSize(model, Pdiff, params.f, params.sigma, modelAdapter);
+    model = setProtPoolSize(model, Pdiff, f, params.sigma, modelAdapter);
     sol = solveLP(model);
     if isempty(sol.x)
-        error(['Changing protein pool to ' num2str(Pdiff * params.f, params.sigma) ' resuls in a non-functional model'])
+        error(['Changing protein pool to ' num2str(Pdiff * f, params.sigma) ' resuls in a non-functional model'])
     end
 else
     error('The total measured protein mass exceeds the total protein content.')
