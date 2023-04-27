f0308ca405708ae8e55628421e4ed2986e75ef3c
Mihail Anton <mihail.anton@chalmers.se>
2023-04-27 12:13:56 +0200

    refactor: model adapter expects full adapter path and extension

--- src/geckomat/model_adapter/ModelAdapterManager.m
+++ src/geckomat/model_adapter/ModelAdapterManager.m
@@ -1,43 +1,35 @@
 %Abstract Base class for adapters for different species
 classdef ModelAdapterManager 
 	methods(Static)
-        %not really needed, we could access it directly. Not very nice, but practical
         function adapter = getAdapterFromPath(adapterPath, addToMatlabPath)
             if nargin < 2
                 addToMatlabPath = true;
             end
             
-            if endsWith(adapterPath,'.m') % Assume it is the adapter file, not path
-                adapter = feval(adapterPath(1:end-2));
-                return
-            end
-            potentialAdapterFiles = dir(fullfile(adapterPath,'*.m'));
+            [adapterFolder, adapterClassName, extension] = fileparts(adapterPath);
+            if ~strcmp(extension, '.m')
+                error('Please provide the full path to the adapter file, including the file extension.');
+            else
+                s = pathsep;
+                pathStr = [s, path, s];
+                onPath = contains(pathStr, [s, adapterFolder, s], 'IgnoreCase', ispc);
+                % Check if the folder is on the path
+                if ~onPath
+                    if addToMatlabPath
+                        addpath(adapterFolder);
+                        savepath();
+                    else
+                        warning(['The adapter will not be on the MATLAB path, since ' ...
+                                'addToMatlabPath is false and it is not currently on ' ...
+                                'the path. Either set addToMatlabPath to true, fix this' ...
+                                'manually before calling this function or make sure it ' ...
+                                'is in current directory (not recommended). If the ' ...
+                                'class is not reachable throughout the entire GECKO use ' ...
+                                'there will be an error.']);
+                    end
+                end
 
-            %Check that there is only one .m file
-            if length(potentialAdapterFiles) ~= 1
-                error('getAdapterFromPath: One, and only one .m file is expected in the folder. This file is expected to contain a ModelAdapter.');
             end
-            %Check if the folder is on the path
-            s = pathsep;
-            pathStr = [s, path, s];
-            onPath = contains(pathStr, [s, adapterPath, s], 'IgnoreCase', ispc);
-            
-            if ~onPath %otherwise do nothing
-               if addToMatlabPath
-                   addpath(adapterPath);
-                   savepath();
-               else
-                   warning(['The adapter will not be on the MATLAB path, since ' ...
-                           'addToMatlabPath is false and it is not currently on ' ...
-                           'the path. Either set addToMatlabPath to true, fix this' ...
-                           'manually before calling this function or make sure it ' ...
-                           'is in current directory (not recommended). If the ' ...
-                           'class is not reachable throughout the entire GECKO use ' ...
-                           'there will be an error.']);
-               end
-            end
-            
-            adapterClassName = extractBefore(potentialAdapterFiles.name,length(potentialAdapterFiles.name)-1);
             adapter = feval(adapterClassName);
         end
         
@@ -51,14 +43,14 @@ classdef ModelAdapterManager
         
         function setDefaultAdapterFromPath(adapterPath, addToMatlabPath)
             if nargin < 2
-                addToMatlabPath = [];
+                addToMatlabPath = true;
             end
             ModelAdapterManager.setDefaultAdapter(ModelAdapterManager.getAdapterFromPath(adapterPath, addToMatlabPath));
         end
         
     end
 	methods(Static,Access = private)
-        %This is how they recommend defining static variables in matlab. Strange, but works
+        % This is how they recommend defining static variables in Matlab
         function out = setGetDefaultAdapter(val)
             persistent defaultAdapter; %will be empty initially
             if nargin
