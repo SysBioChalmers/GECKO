a25a27adec5ba48c31103f6a084d89438a3a5cfd
Eduard Kerkhoven <eduardk@chalmers.se>
2024-05-27 00:51:07 +0200

    fix: updateProtPool obsolete

--- src/geckomat/limit_proteins/updateProtPool.m
+++ src/geckomat/limit_proteins/updateProtPool.m
@@ -1,7 +1,12 @@
 function ecModel  = updateProtPool(ecModel, Ptot, modelAdapter)
 % updateProtPool
-%   Updates the protein pool to compensate for measured proteomics data (in
-%   model.ec.concs).
+%   Obsolete since GECKO 3.2.0, as all (measured and unmeasured) enzymes
+%   are drawn from the protein pool. Instead, use setProtPoolSize. See
+%   https://github.com/SysBioChalmers/GECKO/issues/375 for explanation.
+%
+%   Before GECKO 3.2.0: updates the protein pool to compensate for measured
+%   proteomics data (in model.ec.concs), as only the unmeasured enzymes
+%   draw from the protein pool.
 %
 % Input:
 %   ecModel         an ecModel in GECKO 3 format (with ecModel.ec structure)
@@ -11,11 +16,30 @@ function ecModel  = updateProtPool(ecModel, Ptot, modelAdapter)
 %                   is provided, the modelAdapter value is used.
 %   modelAdapter    a loaded model adapter (Optional, will otherwise use the
 %                   default model adapter).
+%
 % Output:
 %   model           an ecModel where model.ec.concs is populated with
 %                   protein concentrations
 % Usage:
 %   ecModel  = updateProtPool(ecModel, Ptot, modelAdapter)
+
+% Do not run from GECKO version 3.2.0 onwards. This can be recognized by
+% prot_usage reactions that are constrained by proteomics and still draw
+% from the protein pool
+protRxns = find(startsWith(ecModel.rxns,'usage_prot_'));
+poolMetIdx = find(strcmp(ecModel.mets,'prot_pool'));
+% Selected proteins with proteomics constraints
+constProtRxns = ~(ecModel.lb(protRxns)==-1000);
+% Are any still drawing from prot_pool? This is introduced in GECKO 3.2.0.
+if any(full(ecModel.S(poolMetIdx,protRxns(constProtRxns))))
+    error(['In the provided ecModel, all protein usage reactions, both with ' ...
+        'and without concentration constraints, draw from the protein pool. ' ...
+        'This was introduced with GECKO 3.2.0. Since then, updateProtPool ' ...
+        'has become obsolete, use setProtPoolSize instead to constrain the ' ...
+        'total protein pool with  condition-specific total protein content. See '...
+        '<a href="https://github.com/SysBioChalmers/GECKO/issues/375">here</a> ' ...
+        'for more explanation.'])
+end
 
 if nargin < 3 || isempty(modelAdapter)
     modelAdapter = ModelAdapterManager.getDefault();
@@ -37,7 +61,7 @@ end
 
 Pmeas = sum(ecModel.ec.concs,'omitnan');
 if Pmeas == 0
-    error('The model has not yet been constrained with proteomics, as ecModel.ec.concs is empty.')
+    error('The model has not yet been populated with proteomics, as ecModel.ec.concs is empty.')
 end
 
 Pnew = (Ptot - Pmeas) * params.f;
