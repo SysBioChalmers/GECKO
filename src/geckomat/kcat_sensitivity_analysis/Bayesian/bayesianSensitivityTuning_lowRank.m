a1a9436625ece47032ecb64c209a53c914ee941d
Eduard Kerkhoven <eduardk@chalmers.se>
2026-02-10 23:51:46 +0100

    feat: three alternative bayesianSensTune.

--- src/geckomat/kcat_sensitivity_analysis/Bayesian/bayesianSensitivityTuning_lowRank.m
+++ src/geckomat/kcat_sensitivity_analysis/Bayesian/bayesianSensitivityTuning_lowRank.m
@@ -95,7 +95,7 @@ tmpModel = setParam(ecModel,'lb',modelAdapter.params.c_source,0);
 U_r = [];              % D×r PCA basis
 sqrtLambda_r = [];     % r×r diagonal matrix with sqrt eigenvalues (inflated)
 rEff = 0;              % effective rank
-useLowRankKernel = false;
+useLowRankKernel = true;
 
 %% MAIN ABC-SMC LOOP
 generation = 1;
