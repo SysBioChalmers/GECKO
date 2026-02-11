090b35f3d8f8572207e5d4cb067759ce54274bc8
Eduard Kerkhoven <eduardk@chalmers.se>
2026-02-11 07:40:01 +0100

    feat: varFrac parameter

--- src/geckomat/kcat_sensitivity_analysis/Bayesian/bayesianSensitivityTuning_newInitialSample.m
+++ src/geckomat/kcat_sensitivity_analysis/Bayesian/bayesianSensitivityTuning_newInitialSample.m
@@ -95,7 +95,7 @@ tmpModel = setParam(ecModel,'lb',modelAdapter.params.c_source,0);
 U_r = [];              % D×r PCA basis
 sqrtLambda_r = [];     % r×r diagonal matrix with sqrt eigenvalues (inflated)
 rEff = 0;              % effective rank
-useLowRankKernel = true;
+useLowRankKernel = true; varFrac = 0.95;
 
 %% MAIN ABC-SMC LOOP
 generation = 1;
